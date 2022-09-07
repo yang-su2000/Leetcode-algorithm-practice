084c8ffe9d97ec2c60a19e8eff04735a8c4c4880
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2022-09-07 16:53:01 -0400

    Time: 639 ms (27.70%), Space: 17.2 MB (80.71%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.py
+++ 706-design-hashmap/706-design-hashmap.py
@@ -1,17 +1,30 @@
 class MyHashMap:
 
     def __init__(self):
-        self.d = {}
+        self.hash = 2069
+        self.d = [[] for _ in range(self.hash)]
 
     def put(self, key: int, value: int) -> None:
-        self.d[key] = value
+        ls = self.d[key % self.hash]
+        for i, (k, v) in enumerate(ls):
+            if k == key:
+                ls[i] = (key, value)
+                return
+        ls.append((key, value))
 
     def get(self, key: int) -> int:
-        return self.d[key] if key in self.d else -1
+        ls = self.d[key % self.hash]
+        for k, v in ls:
+            if k == key:
+                return v
+        return -1
 
     def remove(self, key: int) -> None:
-        if key in self.d:
-            del self.d[key]
+        ls = self.d[key % self.hash]
+        for i, (k, v) in enumerate(ls):
+            if k == key:
+                ls.pop(i)
+                return
 
 
 # Your MyHashMap object will be instantiated and called as such:
