0ebcf2d972d2dba97cea239bae0c38d7af4f45c5
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2022-09-10 19:30:35 -0400

    Time: 191 ms (58.02%), Space: 13.9 MB (94.99%) - LeetHub

---
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        minCosts = [1001 for _ in range(k)]
+        maxProfits = [0 for _ in range(k)]
+        for val in prices:
+            for i in range(k):
+                minCosts[i] = min(minCosts[i], val - (0 if i == 0 else maxProfits[i-1]))
+                maxProfits[i] = max(maxProfits[i], val - minCosts[i])
+        return maxProfits[-1] if k else 0
