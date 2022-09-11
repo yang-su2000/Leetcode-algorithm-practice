51882c38fb24aa7e7e057c9f3c524968c6b9d6c7
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2022-09-11 13:19:37 -0400

    Time: 747 ms (56.46%), Space: 14.2 MB (91.04%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.py
+++ 621-task-scheduler/621-task-scheduler.py
@@ -1,22 +1,11 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        if not n:
-            return len(tasks)
-        # priority (lowest wait time, highest remaining tasks count)
-        d = defaultdict(lambda: 0)
+        counts = [0] * 26
         for t in tasks:
-            d[t] += 1
-        pq = []
-        for k, v in d.items():
-            heapq.heappush(pq, (0, -v))
-        ans = 0
-        while pq:
-            # print(pq)
-            time, count = heapq.heappop(pq)
-            ans += time + 1
-            for i in range(len(pq)):
-                pq[i] = (max(0, pq[i][0] - time - 1), pq[i][1])
-            if count + 1 != 0:
-                pq.append((n, count + 1))
-            heapq.heapify(pq)
-        return ans
+            counts[ord(t) - ord('A')] += 1
+        counts.sort()
+        max_count = counts.pop()
+        idle = (max_count - 1) * n
+        while counts and idle > 0:
+            idle -= min(max_count - 1, counts.pop())
+        return max(0, idle) + len(tasks)
