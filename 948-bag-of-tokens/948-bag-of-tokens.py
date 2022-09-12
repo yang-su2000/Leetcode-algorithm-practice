b26996b031b0fc45c3c4cb90ef121c81a716166b
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2022-09-11 23:19:11 -0400

    Time: 118 ms (14.15%), Space: 13.9 MB (99.06%) - LeetHub

--- 948-bag-of-tokens/948-bag-of-tokens.py
+++ 948-bag-of-tokens/948-bag-of-tokens.py
@@ -1,15 +1,18 @@
 class Solution:
     def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
-        q = deque(sorted(tokens))
+        tokens.sort()
         score = 0
         ans = 0
-        while q:
-            if power >= q[0]:
-                power -= q.popleft()
+        l, r = 0, len(tokens) - 1
+        while l <= r:
+            if power >= tokens[l]:
+                power -= tokens[l]
+                l += 1
                 score += 1
                 ans = max(ans, score)
             elif score > 0:
-                power += q.pop()
+                power += tokens[r]
+                r -= 1
                 score -= 1
             else:
                 break
