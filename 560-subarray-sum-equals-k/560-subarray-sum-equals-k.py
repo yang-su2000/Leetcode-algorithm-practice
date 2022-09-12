3cdfc6e76b619a58eb3820750f05abd6d4f0892d
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2022-09-12 14:57:30 -0400

    Time: 300 ms (98.28%), Space: 16.7 MB (34.42%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
@@ -1,12 +1,15 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         ans = 0
-        d = defaultdict(int)
         sum = 0
-        d[sum] += 1
+        d = {0:1}
         for num in nums:
             sum += num
             # want to find ? = k, cur - ? = prev, so find cur - k = prev
-            ans += d[sum - k]
-            d[sum] += 1
+            if sum - k in d:
+                ans += d[sum - k]
+            if sum in d:
+                d[sum] += 1
+            else:
+                d[sum] = 1
         return ans
