4d34baa2cb5ee6e0f6787c4708cec84da6036e3d
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2022-09-12 22:12:36 -0400

    Time: 63 ms (24.65%), Space: 14 MB (22.32%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.py
+++ 409-longest-palindrome/409-longest-palindrome.py
@@ -1,20 +1,8 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        uppers = [0] * 26
-        lowers = [0] * 26
-        for c in s:
-            if c >= 'a':
-                lowers[ord(c) - ord('a')] += 1
-            else:
-                uppers[ord(c) - ord('A')] += 1
         odd = False
         ans = 0
-        for i in uppers:
-            if i % 2:
-                odd = True
-                i -= 1
-            ans += i
-        for i in lowers:
+        for i in Counter(s).values():
             if i % 2:
                 odd = True
                 i -= 1
