8cb6868d2c3f445377f33f37000569358194f3f0
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2022-09-13 21:44:40 -0400

    Time: 911 ms (92.84%), Space: 86.3 MB (19.63%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
@@ -5,23 +5,17 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def pseudoPalindromicPaths (self, root: Optional[TreeNode]) -> int:
+    def pseudoPalindromicPaths(self, root: Optional[TreeNode]) -> int:
         self.ans = 0
-        self.rec(root, set())
+        self.rec(root, 0)
         return self.ans
     
-    def rec(self, root, s):
+    def rec(self, root, i):
         if not root:
             return
-        if root.val in s:
-            s.remove(root.val)
-        else:
-            s.add(root.val)
-        self.rec(root.left, s)
-        self.rec(root.right, s)
+        i ^= (1 << root.val)
+        self.rec(root.left, i)
+        self.rec(root.right, i)
         if not root.left and not root.right:
-            self.ans += (len(s) <= 1)
-        if root.val in s:
-            s.remove(root.val)
-        else:
-            s.add(root.val)
+            self.ans += (i & (i - 1) == 0)
+        i ^= (1 << root.val)
