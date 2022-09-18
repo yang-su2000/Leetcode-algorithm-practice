880f1540e3340ec94bd5877f1979568424a6909d
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2022-09-17 20:30:44 -0400

    Time: 112 ms (99.56%), Space: 16 MB (81.24%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -2,23 +2,19 @@ class Solution:
     def trap(self, ls: List[int]) -> int:
         n = len(ls)
         ans = 0
-        h = 0
-        val = 0
-        for i in range(n):
-            if ls[i] >= h:
-                ans += val
-                h = ls[i]
-                val = 0
+        left_h, left_sum = 0, 0
+        for h in ls:
+            if h >= left_h:
+                ans += left_sum
+                left_h, left_sum = h, 0
             else:
-                val += h - ls[i]
-        h = 0
-        val = 0
-        for i in range(n-1, -1, -1):
-            if ls[i] > h:
-                ans += val
-                h = ls[i]
-                val = 0
+                left_sum += left_h - h
+        right_h, right_sum = 0, 0
+        for h in ls[::-1]:
+            if h > right_h:
+                ans += right_sum
+                right_h, right_sum = h, 0
             else:
-                val += h - ls[i]
+                right_sum += right_h -h
         return ans
             
