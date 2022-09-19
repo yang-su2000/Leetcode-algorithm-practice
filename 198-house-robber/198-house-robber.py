5c1b6bbe8965a271cab7645624315c842aa354bb
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2022-09-19 14:49:19 -0400

    Time: 52 ms (49.29%), Space: 14 MB (19.55%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -4,12 +4,9 @@ class Solution:
         if n == 1:
             return nums[0]
         ls = [0] * n
-        nls = [0] * n
         ls[0] = nums[0]
-        ls[1] = nums[1]
-        nls[1] = nums[0]
+        ls[1] = max(nums[0], nums[1])
         for i in range(2, n):
-            ls[i] = nls[i-1] + nums[i]
-            nls[i] = max(ls[i-1], nls[i-1])
-        return max(ls[-1], nls[-1])
+            ls[i] = max(ls[i-2] + nums[i], ls[i-1])
+        return ls[-1]
             
