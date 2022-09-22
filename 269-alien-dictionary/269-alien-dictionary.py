3de6594a73faca23dff48349ef7ed1bc3c303edf
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2022-09-21 23:43:02 -0400

    Time: 78 ms (7.27%), Space: 13.9 MB (61.51%) - LeetHub

--- 269-alien-dictionary/269-alien-dictionary.py
+++ 269-alien-dictionary/269-alien-dictionary.py
@@ -1,35 +1,11 @@
 class Solution:
-    def add_rule(self, pre, cur):
-        i, j = 0, 0
-        n, m = len(pre), len(cur)
-        while i < n and j < m:
-            if pre[i] == cur[j]:
-                i += 1
-                j += 1
-            else:
-                self.rules[pre[i]].add(cur[j])
-                return
-        if n > m:
-            self.invalid = True
-    
-    def add_child(self, parent, childs):
-        s = set()
-        while childs:
-            c = childs.pop()
-            if c not in s:
-                s.add(c)
-                if c in self.rules:
-                    for c2 in self.rules[c]:
-                        if c2 not in s:
-                            childs.append(c2)
-        self.childs[parent] = s
-    
     def alienOrder(self, words: List[str]) -> str:
         A = defaultdict(set) # parent -> set(child)
         in_deg = defaultdict(int)
         for word in words:
             for c in word:
                 in_deg[c] = 0
+        # create A and in_deg
         for i in range(1, len(words)):
             done = False
             for c1, c2 in zip(words[i-1], words[i]):
@@ -41,6 +17,7 @@ class Solution:
                     break
             if not done and len(words[i-1]) > len(words[i]):
                     return ''
+        # topological sort
         ans = []
         q = deque([c for c in in_deg if in_deg[c] == 0])
         while q:
