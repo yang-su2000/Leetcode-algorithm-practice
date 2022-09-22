45ea7ebf1cb5d4b7a82d4d6a6beb3927a347b2e7
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2022-09-22 00:57:30 -0400

    Time: 57 ms (42.58%), Space: 14 MB (23.44%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
@@ -8,12 +8,12 @@ class Solution:
     def foo(self, root, level):
         if not root:
             return
-        if level not in self.d:
-            self.d[level] = root.val
+        if level == len(self.ans):
+            self.ans.append(root.val)
         self.foo(root.right, level + 1)
         self.foo(root.left, level + 1)
     
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        self.d = {}
+        self.ans = []
         self.foo(root, 0)
-        return [val for _, val in sorted(self.d.items())]
+        return self.ans
