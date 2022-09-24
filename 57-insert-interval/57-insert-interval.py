e5e383c5c42268e557feba0088d08736b1696fdb
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2022-09-24 00:04:50 -0400

    Time: 83 ms (95.16%), Space: 17.5 MB (14.87%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -1,33 +1,14 @@
 class Solution:
     def insert(self, ls: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         i, j = newInterval
-        ans = []
-        done = False
+        l, r = [], []
         for a, b in ls:
-            if done:
-                ans.append([a, b])
-                continue
-            if i < a: # [i, a-b]
-                if j < a: # [i-j], [a-b]
-                    ans.append([i, j])
-                    ans.append([a, b])
-                    done = True
-                elif j <= b: # [i, a-j-b]
-                    ans.append([i, b])
-                    done = True
-                else: # [i, a-b, j]
-                    continue
-            elif i <= b: # [a-i-b]
-                if j <= b: # [a-i-j-b]
-                    ans.append([a, b])
-                    done = True
-                else: # [a-i-b, j]
-                    i = a
-                    continue
-            else: # [a-b, i]
-                ans.append([a, b])
-                continue
-        if not done:
-            ans.append([i, j])
-        return ans
+            if b < i:
+                l.append([a, b])
+            elif a > j:
+                r.append([a, b])
+            else:
+                i = min(i, a)
+                j = max(j, b)
+        return l + [[i, j]] + r
             
