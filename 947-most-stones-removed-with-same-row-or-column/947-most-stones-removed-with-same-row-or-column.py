67271672b254d04e766d3e3ed4501ee4919e2d32
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2022-09-25 23:59:19 -0400

    Time: 7300 ms (5.04%), Space: 14.5 MB (94.07%) - LeetHub

---
+++ 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.py
@@ -0,0 +1,26 @@
+class Union:
+    def __init__(self, size):
+        self.parent = [i for i in range(size)]
+        
+    def find(self, i):
+        if self.parent[i] != i:
+            self.parent[i] = self.find(self.parent[i])
+        return self.parent[i]
+    
+    def connect(self, i, j):
+        i, j = self.find(i), self.find(j)
+        self.parent[i] = j
+
+class Solution:
+    def removeStones(self, stones: List[List[int]]) -> int:
+        n = len(stones)
+        U = Union(n)
+        for i in range(n):
+            si = stones[i]
+            for j in range(n):
+                sj = stones[j]
+                if si[0] == sj[0] or si[1] == sj[1]:
+                    U.connect(i, j)
+        for i in range(n):
+            U.find(i)
+        return n - len(set(U.parent))
