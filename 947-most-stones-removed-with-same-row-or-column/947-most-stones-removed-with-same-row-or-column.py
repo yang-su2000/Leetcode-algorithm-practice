36e7d8dda8e17048c0471c07f4ee9b0b1c1504b8
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2022-09-26 00:35:30 -0400

    Time: 8419 ms (5.04%), Space: 14.5 MB (94.07%) - LeetHub

--- 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.py
+++ 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.py
@@ -1,20 +1,23 @@
 class Union:
     def __init__(self, size):
         self.parent = [i for i in range(size)]
-        self.size = [1] * size
         
     def find(self, i):
+        # root = i
+        # while self.parent[root] != root:
+        #     root = self.parent[root]
+        # while i != root:
+        #     parent_i = self.parent[i]
+        #     self.parent[parent_i] = root
+        #     i = parent_i
+        # return root
         if self.parent[i] != i:
             self.parent[i] = self.find(self.parent[i])
         return self.parent[i]
     
     def connect(self, i, j):
         i, j = self.find(i), self.find(j)
-        if self.size[i] > self.size[j]:
-            self.size[i] += self.size[j]
-            self.parent[j] = i
-        else:
-            self.size[j] += self.size[i]
+        if i != j:
             self.parent[i] = j
 
 class Solution:
@@ -27,6 +30,8 @@ class Solution:
                 sj = stones[j]
                 if si[0] == sj[0] or si[1] == sj[1]:
                     U.connect(i, j)
+        # print(U.parent)
         for i in range(n):
             U.find(i)
+        # print(U.parent)
         return n - len(set(U.parent))
