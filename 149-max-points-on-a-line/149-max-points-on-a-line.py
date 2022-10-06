19d32b5b25c26baf18e507f36af0a44d865e1c19
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2022-10-06 19:25:27 -0400

    Time: 91 ms (92.41%), Space: 13.8 MB (93.14%) - LeetHub

--- 149-max-points-on-a-line/149-max-points-on-a-line.py
+++ 149-max-points-on-a-line/149-max-points-on-a-line.py
@@ -3,13 +3,16 @@ class Solution:
         n = len(points)
         if n <= 2:
             return n
-        ls = [points[i] + points[j] for i in range(n) for j in range(i+1, n)]
         ans = 0
-        for line in ls:
-            cur = 0
-            dx2, dy2 = line[2] - line[0], line[3] - line[1]
-            for p in points:
-                dx1, dy1 = p[0] - line[0], p[1] - line[1]
-                cur += (dx1 * dy2 == dy1 * dx2)
-            ans = max(ans, cur)
+        for i in range(n):
+            d = {13141516: 1}
+            for j in range(i+1, n):
+                # if points[j][0] == points[i][0] and points[j][1] == points[i][1]:
+                #     exit(1)
+                slope = (points[j][1] - points[i][1]) / (points[j][0] - points[i][0]) \
+                    if points[j][0] != points[i][0] else 13141516
+                if slope not in d:
+                    d[slope] = 1
+                d[slope] += 1
+            ans = max(ans, max(d.values()))
         return ans
