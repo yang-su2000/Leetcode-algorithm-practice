8d933f9bb4b3c95fc3803ae1169880a887c1be73
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2022-10-13 00:25:31 -0400

    Time: 1871 ms (19.19%), Space: 59.9 MB (77.07%) - LeetHub

--- 0716-max-stack/0716-max-stack.py
+++ 0716-max-stack/0716-max-stack.py
@@ -1,88 +1,32 @@
-from sortedcontainers import SortedDict
-
-class Node:
-    
-    def __init__(self, val):
-        self.l = None
-        self.r = None
-        self.val = val
-        
-    # assume l and r both non-empty
-    def delete(self):
-        self.l.r = self.r
-        self.r.l = self.l
-        return self.val
+from sortedcontainers import SortedList
 
 class MaxStack:
 
     def __init__(self):
-        self.head = Node(0)
-        self.tail = Node(0)
-        self.head.r = self.tail
-        self.tail.l = self.head
-        self.d = SortedDict()
-        self.b = 0 # debug
-
-    def add_tail(self, val):
-        l = self.tail.l
-        r = self.tail
-        node = Node(val)
-        node.l = l
-        l.r = node
-        r.l = node
-        node.r = r
-        return node
-        
-    def remove_tail(self):
-        node = self.tail.l
-        return node.delete()
+        self.s = SortedList() # (idx, val)
+        self.v = SortedList() # (val, idx)
+        self.c = 0
         
     def push(self, x: int) -> None:
-        node = self.add_tail(x)
-        if x in self.d:
-            self.d[x].append(node)
-        else:
-            self.d[x] = [node]
-        if self.b:
-            print('push')
-            self.debug()
+        self.s.add((self.c, x))
+        self.v.add((x, self.c))
+        self.c += 1
 
     def pop(self) -> int:
-        val = self.remove_tail()
-        nodes = self.d[val]
-        node = nodes.pop()
-        if not nodes:
-            del self.d[val]
-        del node
-        if self.b:
-            print('pop')
-            self.debug()
+        i, val = self.s.pop()
+        self.v.remove((val, i))
         return val
 
     def top(self) -> int:
-        node = self.tail.l
-        return node.val
+        return self.s[-1][1]
 
     def peekMax(self) -> int:
-        val, nodes = self.d.peekitem()
-        return val
+        return self.v[-1][0]
 
     def popMax(self) -> int:
-        val, nodes = self.d.peekitem()
-        node = nodes.pop()
-        if not nodes:
-            del self.d[val]
-        node.delete()
-        del node
-        if self.b:
-            print('popMax')
-            self.debug()
+        val, i = self.v.pop()
+        self.s.remove((i, val))
         return val
-                
-    def debug(self):
-        for val, nodes in self.d.items():
-            print(val, 'x', len(nodes), end=' | ')
-        print()
 
 
 # Your MaxStack object will be instantiated and called as such:
