849f2d184a370e57372d0b823b38c6d63cb933de
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2022-10-22 02:42:16 -0400

    Time: 345 ms (14.03%), Space: 16 MB (16.58%) - LeetHub

--- 0296-best-meeting-point/0296-best-meeting-point.py
+++ 0296-best-meeting-point/0296-best-meeting-point.py
@@ -10,16 +10,11 @@ class Solution:
         mls.sort()
         # print(nls, mls)
         if len(nls) % 2:
-            ni = [nls[len(nls)//2]]
-            mi = [mls[len(mls)//2]]
+            ni = nls[len(nls)//2]
+            mi = mls[len(mls)//2]
         else:
-            nval = nls[len(nls)//2-1] + nls[len(nls)//2]
-            mval = mls[len(mls)//2-1] + mls[len(mls)//2]
-            ni = [nval//2, nval//2+1] if nval % 2 else [nval//2]
-            mi = [mval//2, mval//2+1] if mval % 2 else [mval//2]
+            ni = (nls[len(nls)//2-1] + nls[len(nls)//2]) // 2
+            mi = (mls[len(mls)//2-1] + mls[len(mls)//2]) // 2
         # print(ni, mi)
-        ans = float('inf')
-        for i in ni:
-            for j in mi:
-                ans = min(ans, sum([abs(i - nls[k]) + abs(j - mls[k]) for k in range(len(nls))]))
+        ans = sum([abs(ni - nls[k]) + abs(mi - mls[k]) for k in range(len(nls))])
         return ans
