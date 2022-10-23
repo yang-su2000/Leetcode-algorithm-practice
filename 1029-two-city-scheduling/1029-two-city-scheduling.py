32e3d898005e882787dfd762ebc268b444879ea8
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2022-10-23 19:57:24 -0400

    Time: 205 ms (5.12%), Space: 13.8 MB (77.91%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.py
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -5,12 +5,10 @@ class Solution:
         d[(0, 0)] = 0
         for c0, c1 in costs:
             d2 = defaultdict(lambda: float('inf'))
-            d2[(0, 0)] = 0
             for (p0, p1), cost in d.items():
                 if p0 < n:
                     d2[(p0 + 1, p1)] = min(d2[(p0 + 1, p1)], cost + c0)
                 if p1 < n:
                     d2[(p0, p1 + 1)] = min(d2[(p0, p1 + 1)], cost + c1)
             d = d2
-            # print(d.items())
         return d[(n, n)]
