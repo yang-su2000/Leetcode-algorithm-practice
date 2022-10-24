e9c6579d990474757bcd000638298606fc401040
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2022-10-23 20:01:42 -0400

    Time: 80 ms (47.70%), Space: 14 MB (8.62%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.py
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -1,14 +1,7 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
         n = len(costs) // 2
-        d = defaultdict(lambda: float('inf'))
-        d[(0, 0)] = 0
-        for c0, c1 in costs:
-            d2 = defaultdict(lambda: float('inf'))
-            for (p0, p1), cost in d.items():
-                if p0 < n:
-                    d2[(p0 + 1, p1)] = min(d2[(p0 + 1, p1)], cost + c0)
-                if p1 < n:
-                    d2[(p0, p1 + 1)] = min(d2[(p0, p1 + 1)], cost + c1)
-            d = d2
-        return d[(n, n)]
+        costs.sort(key=lambda c: c[0] - c[1])
+        c0 = sum(c[0] for c in costs[:n])
+        c1 = sum(c[1] for c in costs[n:])
+        return c0 + c1
