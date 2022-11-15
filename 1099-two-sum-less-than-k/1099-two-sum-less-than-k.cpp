1dbbb0f236e1ffaa3fa2e1dfa77ce2e39428c46c
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2022-11-15 00:16:38 -0500

    Time: 7 ms (64.08%), Space: 9.2 MB (23.28%) - LeetHub

--- 1099-two-sum-less-than-k/1099-two-sum-less-than-k.cpp
+++ 1099-two-sum-less-than-k/1099-two-sum-less-than-k.cpp
@@ -3,13 +3,10 @@ public:
     int twoSumLessThanK(vector<int>& nums, int k) {
         int sum = -1;
         int n = nums.size();
-        for (int i=0; i<n; i++) {
-            for (int j=i+1; j<n; j++) {
-                int cur = nums[i] + nums[j];
-                if (cur > sum and cur < k) {
-                    sum = cur;
-                }
-            }
+        sort(nums.begin(), nums.end());
+        for (int i=0; i<n and nums[i] < k; i++) {
+            int j = lower_bound(nums.begin()+i+1, nums.end(), k-nums[i]) - (nums.begin()+1);
+            if (j > i) sum = max(sum, nums[i] + nums[j]);
         }
         return sum;
     }
