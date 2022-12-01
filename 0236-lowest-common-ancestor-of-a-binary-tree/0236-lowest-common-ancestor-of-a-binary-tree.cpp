db9036f7aea9f608b85b5ac8b5926f57e2a0589a
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2022-11-30 20:23:53 -0500

    Time: 25 ms (72.46%), Space: 16.4 MB (14.55%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -8,26 +8,33 @@
  * };
  */
 class Solution {
-    TreeNode* ans = NULL;
-    int pv, qv;
-public:
-    // 0: unfound, 1: found p, 2: found q, 3: found both
-    int rec(TreeNode* root) {
-        if (!root) return 0;
-        if (ans) return 3;
-        int ret = rec(root->left) + rec(root->right);
-        if (ans) return 3;
-        if (root->val == pv) ret += 1;
-        if (root->val == qv) ret += 2;
-        if (ret == 3) ans = root;
-        // cout << root->val << "," << ret << endl;
-        return ret;
-    }
-    
+public:    
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        pv = p->val;
-        qv = q->val;
-        rec(root);
-        return ans;
+        stack<TreeNode*> s;
+        s.push(root);
+        // node->val, val = 0 (unfound), 1 (found p), 2 (found q), 3 (found both)
+        unordered_map<TreeNode*, int> m; 
+        while (!s.empty()) {
+            TreeNode* node = s.top();
+            if (!node) {
+                s.pop();
+                continue;
+            }
+            if (!m.count(node)) {
+                m[node] = 0;
+                s.push(node->left);
+                s.push(node->right);
+                continue;
+            }
+            int val = m[node->left] + m[node->right];
+            if (node == p) val += 1;
+            if (node == q) val += 2;
+            if (val == 3) return node;
+            m.erase(node->left);
+            m.erase(node->right);
+            m[node] = val;
+            s.pop();
+        }
+        return nullptr;
     }
 };
