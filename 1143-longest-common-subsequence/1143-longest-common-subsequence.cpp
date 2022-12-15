a2f216c1608b2bc60eacf5e5a2077a95caf80adb
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2022-12-15 00:11:39 -0500

    Time: 16 ms (87.46%), Space: 6.5 MB (91.51%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,39 +1,26 @@
-class Trie {
-    bool tail;
-    unordered_map<char, Trie*> m;
-    
-    Trie(): tail {false} {}
-    
-    void find(string &s, int i) {
-        if (i == s.length()) {
-            tail = true;
-            return;
-        }
-        m[s[i]]->find(s, i+1);
-    }
-};
-
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
         int n1 = text1.length();
         int n2 = text2.length();
-        int i, j, cur;
-        vector<vector<int>> dp(n1, vector<int>(n2));
+        int i, j, val;
+        vector<int> prev(n2);
+        vector<int> cur(n2);
         for (i=0; i<n1; i++) {
             for (j=0; j<n2; j++) {
-                cur = 0;
-                if (i>0) cur = max(cur, dp[i-1][j]);
-                if (j>0) cur = max(cur, dp[i][j-1]);
+                val = 0;
+                if (i>0) val = max(val, prev[j]);
+                if (j>0) val = max(val, cur[j-1]);
                 if (text1[i] == text2[j]) {
-                    if (i>0 and j>0) cur = max(cur, dp[i-1][j-1]+1);
-                    else cur = max(cur, 1);
+                    if (i>0 and j>0) val = max(val, prev[j-1]+1);
+                    else val = max(val, 1);
                 }
-                dp[i][j] = cur;
+                cur[j] = val;
                 // printf("%d ", cur);
             }
+            prev = cur;
             // printf("\n");
         }
-        return dp[n1-1][n2-1];
+        return prev[n2-1];
     }
 };
