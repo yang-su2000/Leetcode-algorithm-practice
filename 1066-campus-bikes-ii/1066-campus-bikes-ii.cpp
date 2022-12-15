e970abc2caf82840e1cb3bfa24f1e0efbb798d34
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2022-12-15 01:56:15 -0500

    Time: 4 ms (92.37%), Space: 8.3 MB (57.63%) - LeetHub

--- 1066-campus-bikes-ii/1066-campus-bikes-ii.cpp
+++ 1066-campus-bikes-ii/1066-campus-bikes-ii.cpp
@@ -1,29 +1,26 @@
 class Solution {
-    int ans = 20001;
+    vector<int> dp; // dp[bit(101)] = the ans (min dist) for pairing bit(101) in bikes and index(0, 1) in workers
     int n, m;
 public:
-    // a: i, b: used
-    void get_dist(vector<vector<int>>& a, vector<vector<int>>& b, vector<bool>& used, int ai, int bi, int d) {
-        if (ai == n) {
-            ans = min(ans, d);
-            return;
+    int get_dist(vector<vector<int>>& a, vector<vector<int>>& b, int ai, int bi) {
+        if (ai == n) return 0;
+        if (dp[bi] != -1) return dp[bi];
+        int val = 20001;
+        int b2, d;
+        for (int i=0; i<m; i++) {
+            b2 = bi | (1 << i);
+            if (b2 == bi) continue;
+            d = abs(a[ai][0] - b[i][0]) + abs(a[ai][1] - b[i][1]);
+            val = min(val, get_dist(a, b, ai+1, b2) + d);
         }
-        while (used[bi]) bi++;
-        int d2;
-        for (int i=bi; i<m; i++) {
-            if (used[i]) continue;
-            used[i] = true;
-            d2 = abs(a[ai][0] - b[i][0]) + abs(a[ai][1] - b[i][1]);
-            get_dist(a, b, used, ai+1, bi, d+d2);
-            used[i] = false;
-        }
+        dp[bi] = val;
+        return val;
     }
     
     int assignBikes(vector<vector<int>>& workers, vector<vector<int>>& bikes) {
         n = workers.size();
         m = bikes.size();
-        vector<bool> used(n);
-        get_dist(workers, bikes, used, 0, 0, 0);
-        return ans;
+        dp.resize(1 << m, -1);
+        return get_dist(workers, bikes, 0, 0);
     }
 };
