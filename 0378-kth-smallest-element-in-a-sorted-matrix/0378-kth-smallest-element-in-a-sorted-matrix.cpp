bbb8dbef5b0e18ccb27814fdbe3aa1b6c1eddd22
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2022-12-27 18:16:20 -0500

    Time: 47 ms (72.89%), Space: 13.1 MB (94.71%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -4,25 +4,19 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
         int i = 0, j = m-1;
-        int l = 0, r = n*m;
         while (0<=i and i<n and 0<=j and j<m) {
             int target = matrix[i][j];
-            auto pos = getPos(target, matrix);
-            if (pos.first <= k and k <= pos.second) return target;
-            else if (pos.second < k) i++;
+            // l: get number of elements < target in sorted mat
+            // r: get number of elements <= target in sorted mat
+            int l = 0, r = 0;
+            for (auto &v:matrix) {
+                l += lower_bound(v.begin(), v.end(), target) - v.begin();
+                r += upper_bound(v.begin(), v.end(), target) - v.begin();
+            }
+            if (l + 1 <= k and k <= r) return target;
+            else if (r < k) i++;
             else j--;
         }
         return matrix[i][j];
-    }
-    
-    // get number of elements <= target in sorted mat
-    pair<int, int> getPos(int target, vector<vector<int>>& mat) {
-        int ans0 = 0;
-        int ans1 = 0;
-        for (auto &v:mat) {
-            ans0 += lower_bound(v.begin(), v.end(), target) - v.begin();
-            ans1 += upper_bound(v.begin(), v.end(), target) - v.begin();
-        }
-        return {ans0+1, ans1};
     }
 };
