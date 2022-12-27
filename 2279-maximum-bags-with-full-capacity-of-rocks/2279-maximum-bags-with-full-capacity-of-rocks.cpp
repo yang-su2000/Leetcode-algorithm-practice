f059fc3ad9acc6582d2bfbcfbbc1e133ec4028ad
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2022-12-26 21:13:40 -0500

    Time: 188 ms (96.48%), Space: 84.5 MB (98.46%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
-    int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
-        int n = capacity.size();
-        vector<int> v(n);
+    int maximumBags(vector<int>& v, vector<int>& rocks, int r) {
+        int n = v.size();
         int i;
-        for (i=0; i<n; i++) v[i] = capacity[i] - rocks[i];
+        for (i=0; i<n; i++) v[i] -= rocks[i];
         sort(v.begin(), v.end());
         i = 0;
-        while (i < n and v[i] <= additionalRocks) additionalRocks -= v[i++];
+        while (i < n and v[i] <= r) r -= v[i++];
         return i;
     }
 };
