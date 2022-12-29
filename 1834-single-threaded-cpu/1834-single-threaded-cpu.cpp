29537462916f4f4f0e6211b47fddbfcab9a3b7bd
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2022-12-28 20:24:22 -0500

    Time: 590 ms (94.18%), Space: 123.1 MB (87.04%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
@@ -1,33 +1,32 @@
+#define pii pair<int, int>
+
 class Solution {
 public:
     vector<int> getOrder(vector<vector<int>>& tasks) {
         int n = tasks.size();
         vector<int> ans;
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pt; // processingTime, i
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> et; // enqueueTime, i
+        priority_queue<pii, vector<pii>, greater<>> pt; // processingTime, i
+        priority_queue<pii, vector<pii>, greater<>> et; // enqueueTime, i
         for (int i=0; i<n; i++) et.push({tasks[i][0], i});
         long time = 0;
         while (!et.empty() or !pt.empty()) {
-            // no task, move to next task
+            // no task, move to next task, enqueue all tasks with the same enqueueTime
             if (pt.empty()) {
                 time = et.top().first;
-                // printf("%d: ", time);
                 while (!et.empty() and et.top().first == time) {
-                    auto p = et.top();
+                    pii p = et.top();
                     et.pop();
-                    // printf("[%d %d]", p.first, p.second);
                     pt.push({tasks[p.second][1], p.second});
                 }
-                // printf("\n");
             }
-            // has task, do it and process time
-            auto p = pt.top();
+            // has task, do it and add processingTime
+            pii p = pt.top();
             pt.pop();
             time += p.first;
             ans.push_back(p.second);
-            // finish task, load tasks that can be enqueued
+            // finish task, enqueue all tasks that can be enqueued
             while (!et.empty() and et.top().first <= time) {
-                auto p = et.top();
+                pii p = et.top();
                 et.pop();
                 pt.push({tasks[p.second][1], p.second});
             }
