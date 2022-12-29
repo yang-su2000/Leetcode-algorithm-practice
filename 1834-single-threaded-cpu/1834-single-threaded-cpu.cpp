46cba320882bac9cb785d4e69b3c5426abac0baa
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2022-12-28 20:28:12 -0500

    Time: 629 ms (92.86%), Space: 123.1 MB (87.04%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
@@ -5,28 +5,29 @@ public:
     vector<int> getOrder(vector<vector<int>>& tasks) {
         int n = tasks.size();
         vector<int> ans;
-        priority_queue<pii, vector<pii>, greater<>> pt; // processingTime, i
-        priority_queue<pii, vector<pii>, greater<>> et; // enqueueTime, i
+        priority_queue<pii, vector<pii>, greater<pii>> pt; // sorted <processingTime, i>
+        priority_queue<pii, vector<pii>, greater<pii>> et; // sorted <enqueueTime, i>
         for (int i=0; i<n; i++) et.push({tasks[i][0], i});
         long time = 0;
+        pii p;
         while (!et.empty() or !pt.empty()) {
             // no task, move to next task, enqueue all tasks with the same enqueueTime
             if (pt.empty()) {
                 time = et.top().first;
                 while (!et.empty() and et.top().first == time) {
-                    pii p = et.top();
+                    p = et.top();
                     et.pop();
                     pt.push({tasks[p.second][1], p.second});
                 }
             }
             // has task, do it and add processingTime
-            pii p = pt.top();
+            p = pt.top();
             pt.pop();
             time += p.first;
             ans.push_back(p.second);
             // finish task, enqueue all tasks that can be enqueued
             while (!et.empty() and et.top().first <= time) {
-                pii p = et.top();
+                p = et.top();
                 et.pop();
                 pt.push({tasks[p.second][1], p.second});
             }
