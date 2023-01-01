c1f08fb938778774399da78e23f89acad7c78c1c
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2022-12-31 21:39:11 -0500

    Time: 27 ms (96.95%), Space: 13.8 MB (63.45%) - LeetHub

--- 1056-confusing-number/1056-confusing-number.py
+++ 1056-confusing-number/1056-confusing-number.py
@@ -3,7 +3,4 @@ class Solution:
         if set(str(n)) & {'2', '3', '4', '5', '7'}:
             return False
         d = {'0': '0', '1': '1', '6': '9', '8': '8', '9': '6'}
-        s = ''
-        for i in str(n):
-            s += d[i]
-        return s[::-1] != str(n)
+        return ''.join([d[c] for c in str(n)][::-1]) != str(n)
