cc7557748d3bc60b111530a70d072d99d92230d7
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-01-06 23:30:54 -0500

    Time: 91 ms (80.36%), Space: 69.5 MB (12.90%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
         int n = gas.size();
-        int l = 0, r = 0, count = 0, g = 0;
+        int l = 0, r = 0, g = 0;
         bool loopover = false;
         while (l < n) {
-            g += gas[r] - cost[r];
-            r = (r + 1) % n;
-            if (r == 0) loopover = true;
-            count++;
+            g += gas[r] - cost[r++];
+            if (r == n) {
+                loopover = true;
+                r = 0;
+            }
             if (g < 0) {
                 if (loopover) return -1;
                 l = r;
-                count = 0;
                 g = 0;
             } else if (l == r) {
                 return l;
