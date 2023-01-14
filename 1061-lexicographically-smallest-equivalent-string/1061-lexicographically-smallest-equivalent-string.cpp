960de5f25e7af40068822e0496faf15e7cffa0be
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-01-13 22:45:13 -0500

    Time: 6 ms (49.54%), Space: 6.6 MB (29.36%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cpp
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cpp
@@ -1,16 +1,17 @@
 class Union {
-    unordered_map<char, char> parent;
+    vector<char> parent;
 public:
     Union() {
-        for (char c='a'; c!='z'+1; c++) parent[c] = c;
+        parent.resize(26);
+        for (int c=0; c<26; c++) parent[c] = c;
     }
     
-    char find(char c) {
+    char find(int c) {
         if (parent[c] != c) parent[c] = find(parent[c]);
         return parent[c];
     }
     
-    void link(char c1, char c2) {
+    void link(int c1, int c2) {
         c1 = find(c1);
         c2 = find(c2);
         if (c1 > c2) parent[c1] = c2;
@@ -22,9 +23,9 @@ class Solution {
 public:
     string smallestEquivalentString(string s1, string s2, string baseStr) {
         Union* U = new Union();
-        for (int i=0; i<s1.size(); i++) U->link(s1[i], s2[i]);
+        for (int i=0; i<s1.size(); i++) U->link(s1[i]-'a', s2[i]-'a');
         string ans;
-        for (char c: baseStr) ans += U->find(c);
+        for (char c: baseStr) ans += U->find(c-'a') + 'a';
         return ans;
     }
 };
