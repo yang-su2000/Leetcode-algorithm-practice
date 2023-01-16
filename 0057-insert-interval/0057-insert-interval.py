452e0604d97c30e5085b86808c4e777064d023a0
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-01-16 02:39:01 -0500

    Time: 77 ms (95.87%), Space: 17.5 MB (16.67%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -2,24 +2,17 @@ class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         if not intervals:
             return [newInterval]
-        ans = []
-        is_inserted = False
+        left_ans = []
+        right_ans = []
         for interval in intervals:
-            if is_inserted:
-                ans.append(interval)
-            # [left <= right < new_left]
-            elif interval[1] < newInterval[0]:
-                ans.append(interval)
-            # [left <= new_right or right]
-            elif interval[0] <= newInterval[1]:
+            # [right < new_left]
+            if interval[1] < newInterval[0]:
+                left_ans.append(interval)
+            # [new_right < left]
+            elif interval[0] > newInterval[1]:
+                right_ans.append(interval)
+            # [new_left <= right or left <= new_right]
+            else:
                 newInterval[0] = min(newInterval[0], interval[0])
                 newInterval[1] = max(newInterval[1], interval[1])
-            # [new_right < left]
-            else:
-                is_inserted = True
-                ans.append(newInterval)
-                ans.append(interval)
-            # print(ans, newInterval)
-        if not is_inserted:
-            ans.append(newInterval)
-        return ans
+        return left_ans + [newInterval] + right_ans
