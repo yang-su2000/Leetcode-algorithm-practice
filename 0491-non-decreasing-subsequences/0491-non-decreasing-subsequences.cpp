3d5f015f660bd8ed8fb7f0bee20caeff05f29f25
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-01-19 21:42:18 -0500

    Time: 412 ms (9.03%), Space: 70.9 MB (14.45%) - LeetHub

---
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    
+public:
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        set<vector<int>> ans;
+        int n = nums.size();
+        int max_count = pow(2, n);
+        for (int i=0; i<max_count; i++) {
+            vector<int> cur;
+            for (int j=0; j<n; j++) {
+                if (i & (1 << j)) {
+                    if (cur.empty() or cur.back() <= nums[j]) cur.push_back(nums[j]);
+                }
+            }
+            if (cur.size() >= 2) ans.insert(cur);
+        }
+        return vector<vector<int>>(ans.begin(), ans.end());
+    }
+};
