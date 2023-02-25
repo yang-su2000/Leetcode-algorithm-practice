16383ee6bb4cb65a1b084ebf95ee0fbcf375e265
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-02-24 19:25:40 -0500

    Time: 137 ms (71.67%), Space: 93.4 MB (56.82%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        // int s0 = 0; // start state profit, always 0
-        int s1 = INT_MIN; // buy state profit
-        int ans = 0; // sell state profit
+        int b = INT_MIN; // buy state profit
+        int s = 0; // sell state profit
         for (int i: prices) {
-            s1 = max(s1, -i);
-            ans = max(ans, s1 + i);
+            b = max(b, -i);
+            s = max(s, b + i);
         }
-        return ans;
+        return s;
     }
 };
