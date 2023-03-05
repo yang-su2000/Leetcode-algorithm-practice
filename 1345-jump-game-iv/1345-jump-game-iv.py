c12c7c0b666c977dae7ef8c11b1f83f0fb77d999
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-03-04 20:52:18 -0500

    Time: 728 ms (92.03%), Space: 26.9 MB (99.19%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.py
+++ 1345-jump-game-iv/1345-jump-game-iv.py
@@ -1,35 +1,30 @@
 class Solution:
     def minJumps(self, arr: List[int]) -> int:
         n = len(arr)
-        if n == 1: return 0
         dp = [0] * n # 0 unvisit, 1 in visit, 2 done visit
         dp[0] = 1
         q = [0]
         d = defaultdict(list)
         for i in range(n):
             d[arr[i]].append(i)
-        ans = 0
+        ans = -1
         while q:
             # print(q)
             ans += 1
             q2 = []
             for i in q:
+                if i == n-1:
+                    return ans
                 if dp[i] == 2:
                     continue
                 dp[i] = 2
                 if i > 0 and dp[i-1] == 0:
-                    if i-1 == n-1:
-                        return ans
                     q2.append(i-1)
                     dp[i-1] = 1
                 if i < n-1 and dp[i+1] == 0:
-                    if i+1 == n-1:
-                        return ans
                     q2.append(i+1)
                     dp[i+1] = 1
                 for j in d[arr[i]]:
-                    if j == n-1:
-                        return ans
                     if dp[j] == 0:
                         q2.append(j)
                         dp[j] = 1
