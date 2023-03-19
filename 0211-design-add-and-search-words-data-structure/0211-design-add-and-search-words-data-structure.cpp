18ec5b88ad73b237ae8c359e3c068c67f5330715
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-03-18 20:09:30 -0400

    Time: 1429 ms (55.59%), Space: 623.5 MB (16.95%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cpp
@@ -0,0 +1,52 @@
+struct TrieNode {
+    TrieNode(){
+        children = vector<TrieNode*>(26, nullptr);
+    }
+    vector<TrieNode*> children;
+    bool end=false;
+};
+
+class WordDictionary {
+public:
+    
+    TrieNode _dict;
+    WordDictionary() {
+            
+    }
+    
+    void addWord(string word) 
+    {
+        auto node = &_dict;
+        for(int i=0; i<word.length(); i++){
+            if(!node->children[word[i]-'a']) 
+                node->children[word[i]-'a'] = new TrieNode();
+            node = node->children[word[i]-'a'];
+        }
+        node->end=true;
+        
+    }
+    
+    bool search(string word) {
+    
+        return search(word, 0, &_dict);
+    }
+    
+    bool search(string& word, int i, TrieNode* node){
+        
+        if(!node) 
+            return false;
+        
+        if(i==word.length()) 
+            return node->end;
+        
+        if(word[i]!='.') 
+            return search(word, i+1, node->children[word[i]-'a']);
+           
+        bool res=false;
+        for(int j=0; j<26 && !res; j++){
+            res = search(word, i+1, node->children[j]);
+        }
+        
+        return res;            
+    }
+};
