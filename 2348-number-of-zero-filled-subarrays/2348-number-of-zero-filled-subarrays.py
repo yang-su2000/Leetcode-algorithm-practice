46d00a13f29e9932e4e7a4a56b2839de07aef6f1
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-03-20 23:42:40 -0400

    Time: 1040 ms (97.53%), Space: 24.6 MB (78.09%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.py
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.py
@@ -5,7 +5,7 @@ class Solution:
         for i in nums:
             if not i:
                 cur += 1
+                ans += cur
             else:
-                ans += (cur + 1) * cur // 2
                 cur = 0
-        return ans + (cur + 1) * cur // 2
+        return ans
