d72386d1d23eaf6b8d7b2533c15c975ca3bfd4d6
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-03-29 22:09:31 -0400

    Time: 317 ms (8.49%), Space: 14.2 MB (80.56%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.py
+++ 0087-scramble-string/0087-scramble-string.py
@@ -7,14 +7,8 @@ class Solution:
                 if s1[i] == s2[j]:
                     dp[0][i][j] = True
         for L in range(2, n+1):
-            for i in range(n):
-                i2 = i + L - 1
-                if i2 >= n:
-                    break
-                for j in range(n):
-                    j2 = j + L - 1
-                    if j2 >= n:
-                        break
+            for i in range(n-L+1):
+                for j in range(n-L+1):
                     for l in range(1, L):
                         l2 = L - l
                         # print(L, i, j, l)
