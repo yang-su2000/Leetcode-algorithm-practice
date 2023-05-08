a8a0cd5c84b779b8cd1da5c1ae018de80e42615e
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-05-08 18:24:52 -0400

    Time: 60 ms (69.44%), Space: 16.6 MB (22.65%) - LeetHub

--- 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.py
+++ 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.py
@@ -4,9 +4,28 @@ class Solution:
         k = len(mat1[0])
         n = len(mat2[0])
         ans = [[0] * n for _ in range(m)]
+        
         # naive
+        # for i in range(m):
+        #     for j in range(n):
+        #         for k_ in range(k):
+        #             ans[i][j] += mat1[i][k_] * mat2[k_][j]
+        # return ans
+        
+        # sklearn sparse
+        d1 = {}
         for i in range(m):
+            for j in range(k):
+                if mat1[i][j]:
+                    d1[(i, j)] = mat1[i][j]
+        d2 = {}
+        for i in range(k):
             for j in range(n):
-                for k_ in range(k):
-                    ans[i][j] += mat1[i][k_] * mat2[k_][j]
+                if mat2[i][j]:
+                    d2[(i, j)] = mat2[i][j]
+        for (i, j), val in d1.items():
+            for (i2, j2), val2 in d2.items():
+                if j == i2:
+                    ans[i][j2] += val * val2
         return ans
+        
