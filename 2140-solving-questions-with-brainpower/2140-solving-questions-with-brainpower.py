3340a2b2934189808ef20bf75a398b23627d6273
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-05-12 01:15:40 -0400

    Time: 1790 ms (19.34%), Space: 193.5 MB (16.46%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
@@ -3,12 +3,9 @@ class Solution:
         n = len(qs)
         dp = [-1] * n
         
+        @cache
         def rec(i):
             nonlocal n, dp
-            if i >= n:
-                return 0
-            if dp[i] == -1:
-                dp[i] = max(rec(i + 1), qs[i][0] + rec(i + 1 + qs[i][1]))
-            return dp[i]
+            return max(rec(i + 1), qs[i][0] + rec(i + 1 + qs[i][1])) if i < n else 0
             
         return rec(0)
