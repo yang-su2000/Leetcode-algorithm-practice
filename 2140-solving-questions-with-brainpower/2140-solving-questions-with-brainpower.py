326f91f05044a27d458b6e8604b38d1a2f9ef6ea
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-05-12 01:19:06 -0400

    Time: 1824 ms (12.35%), Space: 192.2 MB (17.29%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
@@ -1,11 +1,6 @@
 class Solution:
     def mostPoints(self, qs: List[List[int]]) -> int:
-        n = len(qs)
-        dp = [-1] * n
-        
         @cache
         def rec(i):
-            nonlocal n, dp
-            return max(rec(i + 1), qs[i][0] + rec(i + 1 + qs[i][1])) if i < n else 0
-            
+            return max(rec(i + 1), qs[i][0] + rec(i + 1 + qs[i][1])) if i < len(qs) else 0
         return rec(0)
