8961a425bfbacc68fc5be52d08fe62d33646630f
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-05-24 23:29:37 -0400

    Time: 157 ms (6.78%), Space: 16.7 MB (50.00%) - LeetHub

--- 0837-new-21-game/0837-new-21-game.py
+++ 0837-new-21-game/0837-new-21-game.py
@@ -6,17 +6,13 @@ class Solution:
         val = min(1.0, nk / m)
         if k == 1:
             return val
-        dp = [1.0, val]
-        dsum = [1.0, 1.0 + val]
+        dp = [1.0, 1.0 + val]
         while nk < n:
-            val = 1 / m * dsum[-1]
-            if len(dsum) > m:
-                val -= 1 / m * dsum[-m-1]
-            plen = min(m, nk + 1)
-            if len(dp) < plen:
-                val += 1 / m * (plen - len(dp))
-            dp.append(val)
-            dsum.append(dsum[-1] + val)
+            val = dp[-1]
+            if len(dp) > m:
+                val -= dp[-m-1]
+            val += max(0, min(m, nk + 1) - len(dp))
+            dp.append(val * 1 / m + dp[-1])
             nk += 1
         # print(dp)
-        return dp[-1]
+        return val * 1 / m
