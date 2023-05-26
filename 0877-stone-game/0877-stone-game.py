b8684c2d9544ec427b3056374edc48be92b2d2ca
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-05-26 17:52:01 -0400

    Time: 575 ms (23.30%), Space: 128.5 MB (5.90%) - LeetHub

--- 0877-stone-game/0877-stone-game.py
+++ 0877-stone-game/0877-stone-game.py
@@ -1,19 +1,14 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        n = len(piles)
-        for i in range(n-1):
-            piles[i+1] += piles[i]
-        
         @cache
         def dp(l, r):
-            ret = piles[r]
-            if l > 0:
-                ret -= piles[l-1]
             if l == r:
-                return ret
-            ret -= min(dp(l+1, r), dp(l, r-1))
-            return ret
+                return piles[l]
+            if piles[l] - dp(l+1, r) < piles[r] - dp(l, r-1):
+                return piles[r] + dp(l, r-1)
+            else:
+                return piles[l] + dp(l+1, r)
         
-        return dp(0, n-1)
+        return dp(0, len(piles)-1)
             
             
