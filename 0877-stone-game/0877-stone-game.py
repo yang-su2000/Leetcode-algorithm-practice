8366552bd158c5c3e82ca7c30012f7b1a7b67fff
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-05-26 17:58:01 -0400

    Time: 527 ms (26.80%), Space: 127.2 MB (7.20%) - LeetHub

--- 0877-stone-game/0877-stone-game.py
+++ 0877-stone-game/0877-stone-game.py
@@ -1,14 +1,10 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
         @cache
-        def dp(l, r):
+        def dp(l, r): # my stone - # opponent stone
             if l == r:
                 return piles[l]
-            if piles[l] - dp(l+1, r) < piles[r] - dp(l, r-1):
-                return piles[r] + dp(l, r-1)
-            else:
-                return piles[l] + dp(l+1, r)
+            return max(piles[l] - dp(l+1, r), piles[r] - dp(l, r-1))
         
-        return dp(0, len(piles)-1)
-            
+        return dp(0, len(piles)-1) > 0
             
