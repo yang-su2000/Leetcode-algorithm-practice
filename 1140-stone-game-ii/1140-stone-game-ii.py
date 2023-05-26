43d8f589547b8c714ec021167df98410019593b1
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-05-25 23:35:14 -0400

    Time: 177 ms (87.10%), Space: 17.7 MB (50.69%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.py
+++ 1140-stone-game-ii/1140-stone-game-ii.py
@@ -1,32 +1,18 @@
 class Solution:
     def stoneGameII(self, piles: List[int]) -> int:
         n = len(piles)
-        dp = [[None] * n for _ in range(n)]
-        dsum = [piles[-1]]
         for i in range(n-2, -1, -1):
-            dsum.append(piles[i] + dsum[-1])
-        dsum = dsum[::-1]
-        # print(dsum)
+            piles[i] += piles[i+1]
+        dp = [[0] * n for _ in range(n)]
         
-        def solve(i, m): # (x, stone)
-            nonlocal n, dp, dsum
-            if i >= n:
-                return (0, 0)
+        def solve(i, m):
+            nonlocal n, dp
+            # print(i, m)
             if i + 2*m >= n:
-                return (n-i+1, dsum[i])
+                return piles[i]
             if dp[i][m]:
                 return dp[i][m]
-            dx = 0
-            dstone = 0
-            for x in range(1, 2*m+1):
-                x2, stone2 = solve(i+x, max(m, x))
-                stone = dsum[i] - dsum[i+x]
-                stone += solve(i+x+x2, max(m, x, x2))[1]
-                if stone > dstone:
-                    dstone = stone
-                    dx = x
-            dp[i][m] = (dx, dstone)
-            return (dx, dstone)
+            dp[i][m] = piles[i] - min([solve(i+x, max(m, x)) for x in range(1, 2*m+1)])
+            return dp[i][m]
         
-        x, stone = solve(0, 1)
-        return stone
+        return solve(0, 1)
