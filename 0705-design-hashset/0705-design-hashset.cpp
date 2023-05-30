e72c7961ac5f60b82e0f9e31678d7269773cccbf
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-05-29 20:51:29 -0400

    Time: 96 ms (70.27%), Space: 39.7 MB (93.10%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.cpp
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -1,22 +1,35 @@
 class MyHashSet {
-    vector<bool> ht;
+    vector<vector<bool>> ht;
+    int mod;
+    int d;
+    int r;
 public:
     /** Initialize your data structure here. */
     MyHashSet() {
-        ht.resize(1e6+1);
+        mod = 1e3;
+    }
+    
+    void resize(int key) {
+        d = key / mod;
+        r = key % mod;
+        if (ht.size() <= d) ht.resize(d+1);
+        if (ht[d].size() <= r) ht[d].resize(r+1);
     }
     
     void add(int key) {
-        ht[key]=true;
+        resize(key);
+        ht[d][r] = true;
     }
     
     void remove(int key) {
-        ht[key]=false;
+        resize(key);
+        ht[d][r] = false;
     }
     
     /** Returns true if this set contains the specified element */
     bool contains(int key) {
-        return ht[key];
+        resize(key);
+        return ht[d][r];
     }
 };
 
