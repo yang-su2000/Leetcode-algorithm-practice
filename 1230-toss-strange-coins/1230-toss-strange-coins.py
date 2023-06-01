bdee76a3dad4cec010dc88662377491abe6336c0
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-05-31 20:43:25 -0400

    Time: 1151 ms (49.57%), Space: 53.3 MB (40.17%) - LeetHub

--- 1230-toss-strange-coins/1230-toss-strange-coins.py
+++ 1230-toss-strange-coins/1230-toss-strange-coins.py
@@ -1,17 +1,12 @@
 class Solution:
     def probabilityOfHeads(self, prob: List[float], target: int) -> float:
         n = len(prob)
-        dp = [[0] * (target + 1) for _ in range(n)]
-        base = 1
-        for c in range(n):
-            base *= (1 - prob[c])
-            dp[c][0] = base
-        if target == 0:
-            return dp[-1][0]
+        dp = [[0] * (target + 2) for _ in range(n)]
+        dp[0][0] = 1 - prob[0]
         dp[0][1] = prob[0]
         for c in range(1, n):
-            for t in range(1, target + 1):
-                dp[c][t] = dp[c-1][t] * (1 - prob[c]) + dp[c-1][t-1] * prob[c]
+            for t in range(target + 1):
+                dp[c][t] = dp[c-1][t] * (1 - prob[c]) + (dp[c-1][t-1] if t else 0) * prob[c]
         # print(dp)
-        return dp[-1][-1]
+        return dp[n-1][target]
                 
