6893a1ac910395aa48841534525c975572d55f88
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-06-01 20:20:46 -0400

    Time: 539 ms (89.08%), Space: 16.9 MB (30.34%) - LeetHub

---
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maximumDetonation(self, bombs: List[List[int]]) -> int:
+        n = len(bombs)
+        A = [[] for _ in range(n)]
+        for i in range(n):
+            x, y, r = bombs[i]
+            for j in range(n):
+                if i == j:
+                    continue
+                x2, y2, r2 = bombs[j]
+                if (x - x2) * (x - x2) + (y - y2) * (y - y2) <= r * r:
+                    A[i].append(j)
+        ans = 0
+        
+        def dfs(i, vis):
+            nonlocal A
+            ret = 1
+            for j in A[i]:
+                if not vis[j]:
+                    vis[j] = True
+                    ret += dfs(j, vis)
+            return ret   
+        
+        for i in range(n):
+            vis = [False] * n
+            vis[i] = True
+            ans = max(ans, dfs(i, vis))
+                
+        return ans
