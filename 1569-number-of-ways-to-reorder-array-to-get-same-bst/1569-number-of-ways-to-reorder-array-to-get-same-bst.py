db410a050bb9bda728e1408f573cc86cf18499f8
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-06-16 15:40:33 -0400

    Time: 179 ms (52.33%), Space: 21.8 MB (53.49%) - LeetHub

--- 1569-number-of-ways-to-reorder-array-to-get-same-bst/1569-number-of-ways-to-reorder-array-to-get-same-bst.py
+++ 1569-number-of-ways-to-reorder-array-to-get-same-bst/1569-number-of-ways-to-reorder-array-to-get-same-bst.py
@@ -1,22 +1,15 @@
 class Solution:
     def numOfWays(self, nums: List[int]) -> int:
         n = len(nums)
-        dp = [[1] * (n+1) for _ in range(n+1)]
         mod = 10**9+7
-        for j in range(1, n+1):
-            dp[1][j] = j+1
-            dp[j][1] = j+1
-        for i in range(2, n+1):
-            for j in range(2, n+1):
-                dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % mod
         
         def trace(ls):
-            nonlocal mod, dp
+            nonlocal mod
             if len(ls) < 3:
                 return 1
             l = [i for i in ls if i < ls[0]]
             r = [i for i in ls if i > ls[0]]
-            return (dp[len(l)][len(r)] * trace(l) * trace(r)) % mod
+            return (comb(len(ls) - 1, len(l)) * trace(l) * trace(r)) % mod
         
         ans = trace(nums)
         return (ans - 1) % mod
