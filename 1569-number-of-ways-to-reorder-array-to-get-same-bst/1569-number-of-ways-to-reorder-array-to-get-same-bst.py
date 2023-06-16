e368a379c1f8cc50bab7ccecb6fee7503c9ab875
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-06-16 15:38:03 -0400

    Time: 5212 ms (5.81%), Space: 333 MB (5.81%) - LeetHub

--- 1569-number-of-ways-to-reorder-array-to-get-same-bst/1569-number-of-ways-to-reorder-array-to-get-same-bst.py
+++ 1569-number-of-ways-to-reorder-array-to-get-same-bst/1569-number-of-ways-to-reorder-array-to-get-same-bst.py
@@ -8,15 +8,6 @@ class Node:
     #     return str(self.val) + '[' + str(self.l) + ', ' + str(self.r) + ']'
 
 class Solution:
-    # def __init__(self):
-    #     self.dp = [[1] * 1001 for _ in range(1001)]
-    #     for j in range(1, 1001):
-    #         self.dp[1][j] = j+1
-    #         self.dp[j][1] = j+1
-    #     for i in range(2, 1001):
-    #         for j in range(2, 1001):
-    #             self.dp[i][j] = self.dp[i-1][j] + self.dp[i][j-1]
-    
     def numOfWays(self, nums: List[int]) -> int:
         root = Node(nums[0])
         n = len(nums)
@@ -28,32 +19,14 @@ class Solution:
         for i in range(2, n+1):
             for j in range(2, n+1):
                 dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % mod
-        for i in range(1, n):
-            val = nums[i]
-            node = root
-            while node:
-                if val < node.val:
-                    if node.l:
-                        node = node.l
-                    else:
-                        node.l = Node(val)
-                        break
-                else:
-                    if node.r:
-                        node = node.r
-                    else:
-                        node.r = Node(val)
-                        break
-        # print(root)
         
-        def trace(node):
+        def trace(ls):
             nonlocal mod, dp
-            if not node:
-                return 0, 1
-            count1, ans1 = trace(node.l)
-            count2, ans2 = trace(node.r)
-            return count1 + count2 + 1, (dp[count1][count2] * ans1 * ans2) % mod
+            if not ls:
+                return 1
+            l = [i for i in ls if i < ls[0]]
+            r = [i for i in ls if i > ls[0]]
+            return (dp[len(l)][len(r)] * trace(l) * trace(r)) % mod
         
-        count, ans = trace(root)
-        # print(count, ans)
+        ans = trace(nums)
         return (ans - 1) % mod
