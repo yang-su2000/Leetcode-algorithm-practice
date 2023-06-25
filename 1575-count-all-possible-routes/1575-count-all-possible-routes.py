bdf1af4c26ceaea3ca4cc28c19d845b341021ebf
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-06-25 02:21:27 -0400

    Time: 2081 ms (57.14%), Space: 33.9 MB (31.58%) - LeetHub

--- 1575-count-all-possible-routes/1575-count-all-possible-routes.py
+++ 1575-count-all-possible-routes/1575-count-all-possible-routes.py
@@ -1,9 +1,5 @@
 class Solution:
     def countRoutes(self, ls: List[int], start: int, end: int, fuel: int) -> int:
-        mod = 10 ** 9 + 7
-        
         @cache
-        def dp(x, f):
-            return sum(x == end if z == x else dp(z, f - abs(ls[x] - ls[z])) for z in range(len(ls))) % mod if f >= 0 else 0
-        
+        def dp(a, f): return sum(a == end if a == b else dp(b, f - abs(ls[a] - ls[b])) for b in range(len(ls))) % 1000000007 if f >= 0 else 0
         return dp(start, fuel)
