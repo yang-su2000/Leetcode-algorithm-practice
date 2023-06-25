1be3d8465e3613ccc8a2dd6c5424fb879d1f7e4d
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-06-25 02:24:26 -0400

    Time: 2074 ms (57.89%), Space: 33.8 MB (31.58%) - LeetHub

--- 1575-count-all-possible-routes/1575-count-all-possible-routes.py
+++ 1575-count-all-possible-routes/1575-count-all-possible-routes.py
@@ -1,5 +1,5 @@
 class Solution:
     def countRoutes(self, ls: List[int], a: int, b: int, f: int) -> int:
         @cache
-        def dp(a_, f_): return sum(a_ == b if a_ == b_ else dp(b_, f_ - abs(ls[a_] - ls[b_])) for b_ in range(len(ls))) % 1000000007 if f_ >= 0 else 0
+        def dp(a_, f_): return sum(a_ == b if a_ == i else dp(i, f_ - abs(ls[a_] - ls[i])) for i in range(len(ls))) % 1000000007 if f_ >= 0 else 0
         return dp(a, f)
