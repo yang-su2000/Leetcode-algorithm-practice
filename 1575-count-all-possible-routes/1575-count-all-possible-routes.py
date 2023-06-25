3d7bd05a84c8d5012be5a4f47c3087d62533178b
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-06-25 02:04:04 -0400

    Time: 1983 ms (75.19%), Space: 22.9 MB (50.38%) - LeetHub

--- 1575-count-all-possible-routes/1575-count-all-possible-routes.py
+++ 1575-count-all-possible-routes/1575-count-all-possible-routes.py
@@ -1,22 +1,18 @@
 class Solution:
     def countRoutes(self, ls: List[int], start: int, end: int, fuel: int) -> int:
-        n = len(ls)
         mod = 10 ** 9 + 7
         
         @cache
         def dp(x, f):
-            nonlocal ls, n, mod
-            if f == 0:
+            if f < 0:
                 return 0
-            if abs(ls[x] - ls[end]) > f:
-                return 0
-            ret = (x != end)
-            for z in range(n):
+            ret = (x == end)
+            for z in range(len(ls)):
                 if z == x:
                     continue
                 if abs(ls[x] - ls[z]) <= f:
-                    ret = (ret + dp(z, f - abs(ls[x] - ls[z]))) % mod
+                    ret += dp(z, f - abs(ls[x] - ls[z]))
             # print(x, f, ret)
             return ret % mod
         
-        return dp(start, fuel) + (start == end)
+        return dp(start, fuel)
