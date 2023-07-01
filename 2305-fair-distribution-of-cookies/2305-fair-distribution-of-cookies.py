fd8408cf60b473ab07945067cbeb1567f02be1be
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-06-30 21:18:13 -0400

    Time: 315 ms (60.08%), Space: 16.4 MB (21.27%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
@@ -2,17 +2,20 @@ class Solution:
     def distributeCookies(self, cookies: List[int], k: int) -> int:
         ans = inf
         
-        def bt(i, ls, mval):
+        def bt(i, ls, mval, zero_count):
             nonlocal ans
             if i == len(cookies):
                 ans = min(ans, mval)
                 return
+            if len(cookies) - i < zero_count:
+                return
             if mval >= ans:
                 return
             for j in range(k):
+                zero_count2 = zero_count - (ls[j] == 0)
                 ls[j] += cookies[i]
-                bt(i + 1, ls, max(mval, ls[j]))
+                bt(i + 1, ls, max(mval, ls[j]), zero_count2)
                 ls[j] -= cookies[i]
         
-        bt(0, [0] * k, 0)
+        bt(0, [0] * k, 0, k)
         return ans
