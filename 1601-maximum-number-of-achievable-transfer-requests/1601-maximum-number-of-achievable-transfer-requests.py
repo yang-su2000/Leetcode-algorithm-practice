13b0c64238b7c477bbfeafd71653bec3b97df0fb
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-07-01 20:12:18 -0400

    Time: 2466 ms (26.21%), Space: 16.4 MB (53.40%) - LeetHub

--- 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.py
+++ 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.py
@@ -2,18 +2,17 @@ class Solution:
     def maximumRequests(self, n: int, requests: List[List[int]]) -> int:
         m = len(requests)
         ans = 0
-        for i in range(1 << m):
+        for i in range((1 << m) - 1, 0, -1):
             in_ = [0] * n
             out_ = [0] * n
-            cur = 0
+            cur = bin(i).count('1')
+            if cur < ans:
+                continue
             for j in range(m):
                 if i & (1 << j):
                     a, b = requests[j]
                     in_[b] += 1
                     out_[a] += 1
-                    cur += 1
-            if cur < ans:
-                continue
             if in_ == out_:
                 ans = max(ans, cur)
         return ans
