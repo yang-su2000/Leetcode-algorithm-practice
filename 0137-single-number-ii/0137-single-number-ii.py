e5530ee264f1e9c77b49ee09421d2b004121edd8
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-07-03 21:18:38 -0400

    Time: 67 ms (90.95%), Space: 18 MB (86.89%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,9 +1,11 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        a = 0
+        a = (1 << 32) - 1
         b = 0
         for i in nums:
-            a = (~b) & (a ^ i)
-            b = (~a) & (b ^ i)
-            # print(i, a, b)
-        return a
+            a = (a ^ i) & (~b)
+            b = (b ^ i) & (~a)
+            # print(i, bin(a), bin(b))
+        if b >= (1 << 31):
+            b = b - (1 << 32)
+        return b
