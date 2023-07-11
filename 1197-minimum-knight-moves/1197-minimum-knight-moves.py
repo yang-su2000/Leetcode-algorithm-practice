6fa0805ff2c14a374d1fed061c508ba6a45c67b7
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-07-11 01:19:08 -0400

    Time: 596 ms (75.63%), Space: 19.9 MB (82.50%) - LeetHub

--- 1197-minimum-knight-moves/1197-minimum-knight-moves.py
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.py
@@ -1,22 +1,19 @@
-ans = defaultdict(lambda: inf) # (x, y) -> min
-ans[(0, 0)] = 0
-cur = [(0, 0, 0)]
-d = [(-1, -2), (-1, 2), (-2, 1), (-2, -1), (1, 2), (1, -2), (2, 1), (2, -1)]
-while cur:
-    nxt = []
-    for i, j, val in cur:
-        if ans[(i, j)] < val:
-            continue
-        for di, dj in d:
-            i2, j2 = i + di, j + dj
-            if abs(i2) >= 305 or abs(j2) >= 305:
-                continue
-            if val + 1 < ans[(i2, j2)]:
-                ans[(i2, j2)] = val + 1
-                nxt.append((i2, j2, val + 1))
-    cur = nxt
-
 class Solution:
     def minKnightMoves(self, x: int, y: int) -> int:
         x, y = abs(x), abs(y)
+        ans = defaultdict(lambda: inf) # (x, y) -> min
+        ans[(0, 0)] = 0
+        cur = [(0, 0, 0)]
+        d = [(1, 2), (2, 1), (1, -2), (-1, 2), (-2, 1), (2, -1)]
+        while cur:
+            nxt = []
+            for i, j, val in cur:
+                if ans[(i, j)] < val:
+                    continue
+                for di, dj in d:
+                    i2, j2 = i + di, j + dj
+                    if -3 <= i2 <= x + 3 and -3 <= j2 <= y + 3 and val + 1 < ans[(i2, j2)]:
+                        ans[(i2, j2)] = val + 1
+                        nxt.append((i2, j2, val + 1))
+            cur = nxt
         return ans[(x, y)]
