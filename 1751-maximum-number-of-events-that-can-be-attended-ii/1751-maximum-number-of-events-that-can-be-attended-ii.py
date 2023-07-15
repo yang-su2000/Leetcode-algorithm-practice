c27808669c918e3a39db6112ad131e609c90608a
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-07-14 22:30:33 -0400

    Time: 974 ms (60.00%), Space: 62.4 MB (84.44%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.py
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.py
@@ -2,23 +2,17 @@ from bisect import bisect_right
 
 class Solution:
     def maxValue(self, events: List[List[int]], k: int) -> int:
-        events.sort()
-        # k -> end, sum
-        ls = [(0, 0)]
-        for _ in range(k):
-            ls2 = []
-            for a, b, val in events:
-                p = bisect_right(ls, (a, -inf)) - 1
-                val2 = ls[p][1] + val
-                ls2.append((b, val2))
-            ls2.extend(ls)
-            ls2.sort()
-            ls3 = []
-            for end, sval in ls2:
-                if not ls3 or (ls3[-1][0] < end and ls3[-1][1] < sval):
-                    ls3.append((end, sval))
-                elif ls3[-1][0] == end:
-                    ls3[-1] = (end, max(ls3[-1][1], sval))
-            ls = ls3
-            # print(ls)
-        return max(sval for _, sval in ls)
+        events.sort(key=lambda x: x[1])
+        ends = [end for _, end, _ in events]
+        n = len(events)
+        dp = [[0] * (k + 1) for _ in range(n)]
+        for i in range(n):
+            a, b, val = events[i]
+            pi = bisect_left(ends, a) - 1
+            for j in range(k, 0, -1):
+                not_pick = dp[i-1][j] if i else 0
+                pick = val + (dp[pi][j-1] if pi >= 0 else 0)
+                dp[i][j] = max(not_pick, pick)
+                # print(i, j, pi, not_pick, pick)
+        # print(dp)
+        return dp[-1][-1]
