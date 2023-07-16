485ad9e3e041a52564765f899f2ddb41dbc89b73
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-07-16 01:43:52 -0400

    Time: 168 ms (67.72%), Space: 23.9 MB (45.57%) - LeetHub

--- 1125-smallest-sufficient-team/1125-smallest-sufficient-team.py
+++ 1125-smallest-sufficient-team/1125-smallest-sufficient-team.py
@@ -1,27 +1,16 @@
 class Solution:
     def smallestSufficientTeam(self, req_skills: List[str], people: List[List[str]]) -> List[int]:
-        n = len(req_skills)
-        d = {req_skills[i]: i for i in range(n)}
-        ls = []
-        for skills in people:
-            val = 0
-            for skill in skills:
-                val |= (1 << d[skill])
-            ls.append(val)
-        # print(ls)
-        m = len(ls)
-        dp = [(1 << m) - 1] * (1 << n) # skill_mask -> people_mask
-        dp[0] = 0
-        for skill_mask in range(1, 1 << n): # 2**16
-            for i, p in enumerate(ls): # 60
-                skill_mask2 = skill_mask & ~p
-                people_mask2 = dp[skill_mask2] | (1 << i)
-                if skill_mask2 != skill_mask and people_mask2.bit_count() < dp[skill_mask].bit_count():
-                    dp[skill_mask] = people_mask2
-            # print(dp)
-        res = dp[(1 << n) - 1]
-        ans = []
-        for i in range(m):
-            if res & (1 << i):
-                ans.append(i)
-        return ans
+        n, m = len(req_skills), len(people)
+        skill_index = {v: i for i, v in enumerate(req_skills)}
+        dp = {0: []} # skill_mask -> people_mask
+        for i, p in enumerate(people):
+            cur_skill = 0
+            for skill in p:
+                if skill in skill_index:
+                    cur_skill |= 1 << skill_index[skill]
+            for prev, need in dict(dp).items():
+                comb = prev | cur_skill
+                if comb == prev: continue
+                if comb not in dp or len(dp[comb]) > len(need) + 1:
+                    dp[comb] = need + [i]
+        return dp[(1 << n) - 1]
