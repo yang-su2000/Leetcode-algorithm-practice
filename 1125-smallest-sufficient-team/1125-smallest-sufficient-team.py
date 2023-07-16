595a398041c243c391cceff1ce621312243b9294
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-07-16 01:37:42 -0400

    Time: 1765 ms (12.03%), Space: 18.8 MB (63.29%) - LeetHub

--- 1125-smallest-sufficient-team/1125-smallest-sufficient-team.py
+++ 1125-smallest-sufficient-team/1125-smallest-sufficient-team.py
@@ -9,28 +9,19 @@ class Solution:
                 val |= (1 << d[skill])
             ls.append(val)
         # print(ls)
-        n = (1 << n)
         m = len(ls)
-        dp = [[None] * n for _ in range(m + 1)] # (cur_people, skills) -> (size, set_people)
-        dp[0][0] = (0, 0)
-        for i in range(m):
-            skill = ls[i]
-            for j in range(n):
-                if dp[i][j] is None:
-                    continue
-                val, res = dp[i][j]
-                # pick
-                skill2 = (j | skill)
-                if dp[i+1][skill2] is None or val + 1 < dp[i+1][skill2][0]:
-                    dp[i+1][skill2] = (val + 1, res | (1 << i))
-                # not pick
-                if dp[i+1][j] is None or val < dp[i+1][j][0]:
-                    dp[i+1][j] = (val, res)
-        # for i in range(m + 1):
-        #     print(i, dp[i])
-        res = dp[m][n - 1][1]
-        ret = []
+        dp = [(1 << m) - 1] * (1 << n) # skill_mask -> people_mask
+        dp[0] = 0
+        for skill_mask in range(1, 1 << n): # 2**16
+            for i, p in enumerate(ls): # 60
+                skill_mask2 = skill_mask & ~p
+                people_mask2 = dp[skill_mask2] | (1 << i)
+                if skill_mask2 != skill_mask and people_mask2.bit_count() < dp[skill_mask].bit_count():
+                    dp[skill_mask] = people_mask2
+            # print(dp)
+        res = dp[(1 << n) - 1]
+        ans = []
         for i in range(m):
             if res & (1 << i):
-                ret.append(i)
-        return ret
+                ans.append(i)
+        return ans
