8ce893bd3e944dff9897d1b85c398e2065e89b2c
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-07-18 19:43:55 -0400

    Time: 43 ms (98.98%), Space: 16.4 MB (99.12%) - LeetHub

--- 1197-minimum-knight-moves/1197-minimum-knight-moves.py
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.py
@@ -1,19 +1,13 @@
 class Solution:
     def minKnightMoves(self, x: int, y: int) -> int:
         x, y = abs(x), abs(y)
-        ans = defaultdict(lambda: inf) # (x, y) -> min
-        ans[(0, 0)] = 0
-        cur = [(0, 0, 0)]
-        d = [(1, 2), (2, 1), (1, -2), (-1, 2), (-2, 1), (2, -1)]
-        while cur:
-            nxt = []
-            for i, j, val in cur:
-                if ans[(i, j)] < val:
-                    continue
-                for di, dj in d:
-                    i2, j2 = i + di, j + dj
-                    if -3 <= i2 <= x + 3 and -3 <= j2 <= y + 3 and val + 1 < ans[(i2, j2)]:
-                        ans[(i2, j2)] = val + 1
-                        nxt.append((i2, j2, val + 1))
-            cur = nxt
-        return ans[(x, y)]
+        if x < y:
+            x, y = y, x
+        if x == 1 and y == 0:
+            return 3
+        if x == 2 and y == 2:
+            return 4
+        diff = x - y
+        if diff < y:
+            return diff - (diff - y) // 3 * 2
+        return diff - (diff - y) // 4 * 2
