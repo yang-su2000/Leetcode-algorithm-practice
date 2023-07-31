8990ae241d72a60ba8526d6aaab0275ada526a9f
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-07-30 21:52:51 -0400

    Time: 31 ms (86.16%), Space: 15.2 MB (68.25%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -3,23 +3,24 @@ public:
     int minimumDeleteSum(string s1, string s2) {
         int n1 = s1.length();
         int n2 = s2.length();
-        vector<vector<int>> memo(n1, vector<int>(n2, -1));
-        function<int(int, int)> dp = [&](int i, int j) {
-            int ret = 0;
-            if (i >= n1 || j >= n2) {
-                while (i < n1) ret += s1[i++];
-                while (j < n2) ret += s2[j++];
-                return ret;
+        // at dp[i][j] means uses len(s1) == i, len(s2) == j
+        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1));
+        for (int i=1; i<=n1; i++) {
+            dp[i][0] = s1[i-1] + dp[i-1][0];
+        }
+        for (int j=1; j<=n2; j++) {
+            dp[0][j] = s2[j-1] + dp[0][j-1];
+        }
+        for (int i=1; i<=n1; i++) {
+            for (int j=1; j<=n2; j++) {
+                if (s1[i-1] == s2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    dp[i][j] = min(s1[i-1] + dp[i-1][j], s2[j-1] + dp[i][j-1]);
+                }
+                // cout << i << ", " << j << ": " << dp[i][j] << endl;
             }
-            if (memo[i][j] != -1) return memo[i][j];
-            if (s1[i] == s2[j]) {
-                ret = dp(i + 1, j + 1);
-            } else {
-                ret = min(s1[i] + dp(i + 1, j), s2[j] + dp(i, j + 1));
-            }
-            memo[i][j] = ret;
-            return ret;
-        };
-        return dp(0, 0);
+        }
+        return dp[n1][n2];
     }
 };
