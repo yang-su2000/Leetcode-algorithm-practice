e0fbcbeb207179faf4d6118d2007f2ed1f1a150f
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-07-31 20:56:52 -0400

    Time: 39 ms (31.18%), Space: 18.1 MB (29.10%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -11,6 +11,7 @@ public:
             int cur;
             if (ls.empty()) cur = 1;
             else cur = ls.back() + 1;
+            if (n - cur + 1 + k_ < k) return;
             for (int i=cur; i<=n; i++) {
                 ls.push_back(i);
                 bt(ls);
