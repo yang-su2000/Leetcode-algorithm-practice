db1497f98aa995d0aa5e655663ccd5d336508cd6
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-07-31 21:00:54 -0400

    Time: 3 ms (99.80%), Space: 8.9 MB (83.81%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,30 +1,25 @@
 class Solution {
-    vector<vector<int>> ans;
-    int N;
-    int K;
 public:
-    void bt(vector<int>& ls) {
-        int k_ = ls.size();
-        if (k_ == K) {
-            ans.push_back(ls);
-            return;
-        }
-        int cur;
-        if (ls.empty()) cur = 1;
-        else cur = ls.back() + 1;
-        if (N - cur + 1 + k_ < K) return;
-        for (int i=cur; i<=N; i++) {
-            ls.push_back(i);
-            bt(ls);
-            ls.pop_back();
-        }
-    }
-
     vector<vector<int>> combine(int n, int k) {
-        N = n;
-        K = k;
-        vector<int> ls;
-        bt(ls);
+        vector<vector<int>> ans;
+        function<void(vector<int>&)> bt = [&](vector<int>& ls) {
+            int k_ = ls.size();
+            if (k_ == k) {
+                ans.push_back(ls);
+                return;
+            }
+            int cur;
+            if (ls.empty()) cur = 1;
+            else cur = ls.back() + 1;
+            if (n - cur + 1 + k_ < k) return;
+            for (int i=cur; i<=n; i++) {
+                ls.push_back(i);
+                bt(ls);
+                ls.pop_back();
+            }
+        };
+        vector<int> tmp;
+        bt(tmp);
         return ans;
     }
 };
