b7ff0f42347f7a6bf904f6c2cd4735be409a9eab
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-08-06 01:25:18 -0400

    Time: 147 ms (5.26%), Space: 34.4 MB (6.02%) - LeetHub

--- 0920-number-of-music-playlists/0920-number-of-music-playlists.py
+++ 0920-number-of-music-playlists/0920-number-of-music-playlists.py
@@ -1,6 +1,4 @@
 class Solution:
-    def numMusicPlaylists(self, n: int, goal: int, k: int) -> int:
-        @cache
-        def dp(uplayed, played):
-            return uplayed >= n if played == goal else ((n - uplayed) * dp(uplayed + 1, played + 1) + max(uplayed - k, 0) * dp(uplayed, played + 1)) % (10**9+7)
-        return dp(0, 0)
+    @cache
+    def numMusicPlaylists(self, n: int, goal: int, k: int, played: int = 0) -> int:
+        return played >= n if not goal else ((n - played) * self.numMusicPlaylists(n, goal - 1, k, played + 1) + max(played - k, 0) * self.numMusicPlaylists(n, goal - 1, k, played)) % (10**9+7)
