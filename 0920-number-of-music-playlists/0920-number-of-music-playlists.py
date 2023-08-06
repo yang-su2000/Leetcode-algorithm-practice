bb6657ec5cede7b353b44704ff7bb8190e318447
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-08-06 01:05:34 -0400

    Time: 89 ms (36.84%), Space: 20.6 MB (6.02%) - LeetHub

--- 0920-number-of-music-playlists/0920-number-of-music-playlists.py
+++ 0920-number-of-music-playlists/0920-number-of-music-playlists.py
@@ -1,15 +1,6 @@
 class Solution:
     def numMusicPlaylists(self, n: int, goal: int, k: int) -> int:
-        mod = 10**9+7
-        # number of unique played songs, number of played songs, number of old songs that can be replayed
         @cache
         def dp(uplayed, played, old):
-            if played == goal:
-                ret = uplayed >= n
-            else:
-                new_played = (n - uplayed) * dp(uplayed + 1, played + 1, old + (uplayed >= k))
-                old_played = old * dp(uplayed, played + 1, old)
-                ret = new_played + old_played
-            return ret % mod
-        
+            return uplayed >= n if played == goal else ((n - uplayed) * dp(uplayed + 1, played + 1, old + (uplayed >= k)) + old * dp(uplayed, played + 1, old)) % (10**9+7)
         return dp(0, 0, 0)
