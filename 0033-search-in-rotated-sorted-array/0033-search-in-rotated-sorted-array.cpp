f0b6e031fddec7e6c4b154a91a302c636d8dc903
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-08-07 21:52:56 -0400

    Time: 4 ms (44.66%), Space: 11.1 MB (45.03%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int l=0, r=nums.size();
-        while (l < r) {
+        int l=0, r=nums.size()-1;
+        function<bool(int, int)> same_side = [&](int mid, int target) {
+            if (nums[0] <= mid && nums[0] <= target) return true;
+            if (nums[0] > mid && nums[0] > target) return true;
+            return false;
+        };
+        while (l <= r) {
             int mid = (l + r) / 2;
-            if (target < nums[0] && nums[0] < nums[mid]) l = mid+1;
-            else if (nums[mid] < nums[0] && nums[0] <= target) r = mid;
-            else if (nums[mid] < target) l = mid+1;
-            else if (target < nums[mid]) r = mid;
-            else return mid;
+            int val;
+            if (same_side(nums[mid], target)) val = nums[mid];
+            else if (nums[0] <= target) val = INT_MAX;
+            else val = INT_MIN;
+            if (target == val) return mid;
+            else if (target < val) r = mid-1;
+            else l = mid+1;
         }
         return -1;
     }
