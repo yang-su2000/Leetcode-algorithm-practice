6f20bc80f757501271f769065a4912a1ee1f2433
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-08-13 20:14:29 -0400

    Time: 408 ms (98.63%), Space: 29.9 MB (40.15%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,20 +1,18 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        def quick_select(nums, k):
-            pivot = random.choice(nums)
-            l, m, r = [], [], []
-            for i in nums:
-                if i > pivot:
-                    l.append(i)
-                elif i == pivot:
-                    m.append(i)
-                else:
-                    r.append(i)
-            if k <= len(l):
-                return quick_select(l, k)
-            elif len(l) + len(m) < k:
-                return quick_select(r, k - len(l) - len(m))
+        n = len(nums)
+        pivot = random.choice(nums)
+        l, m, r = [], [], []
+        for i in nums:
+            if i > pivot:
+                l.append(i)
+            elif i == pivot:
+                m.append(i)
             else:
-                return pivot
-        
-        return quick_select(nums, k)
+                r.append(i)
+        if k <= len(l):
+            return self.findKthLargest(l, k)
+        elif k <= len(l) + len(m):
+            return pivot
+        else:
+            return self.findKthLargest(r, k - len(l) - len(m))
