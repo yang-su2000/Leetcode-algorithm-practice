bcde7174b0e5d5a44c3b1d46de7a787d4192adef
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-09-05 18:31:12 -0400

    Time: 2443 ms (78.22%), Space: 57.7 MB (89.11%) - LeetHub

--- 2836-maximize-value-of-function-in-a-ball-passing-game/2836-maximize-value-of-function-in-a-ball-passing-game.py
+++ 2836-maximize-value-of-function-in-a-ball-passing-game/2836-maximize-value-of-function-in-a-ball-passing-game.py
@@ -1,18 +1,24 @@
 class Solution:
     def getMaxFunctionValue(self, childs: List[int], k: int) -> int:
         n = len(childs)
-        res = [(i, i) for i in range(n)] # final (pos, val)
-        dp = [(childs[i], childs[i]) for i in range(n)] # 2^i (pos, val)
+        # res[start pos] = (end pos, val) at k steps
+        res = [(i, i) for i in range(n)]
+        # dp[start pos] = (end pos, val) at each 2^i steps
+        dp = [(childs[p], childs[p]) for p in range(n)]
         for i in range(40):
             if k & (1 << i):
-                for i in range(n):
-                    pos, val = res[i]
-                    pos2, val2 = dp[pos]
-                    res[i] = (pos2, val + val2)
+                for cur_pos in range(n):
+                    start_pos, val = res[cur_pos]
+                    end_pos, val2 = dp[start_pos]
+                    res[cur_pos] = (end_pos, val + val2)
             dp2 = [None] * n
-            for i in range(n):
-                pos, val = dp[i]
-                pos2, val2 = dp[pos]
-                dp2[i] = (pos2, val + val2)
+            for start_pos in range(n):
+                mid_pos, val = dp[start_pos]
+                end_pos, val2 = dp[mid_pos]
+                dp2[start_pos] = (end_pos, val + val2)
             dp = dp2
         return max(val for pos, val in res)
+    
+    
+    
+    
