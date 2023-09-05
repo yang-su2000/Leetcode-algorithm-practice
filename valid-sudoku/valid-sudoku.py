0f5842148ceb864314c074ce83ab0a7f2a7250fa
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-09-05 00:41:21 -0400

    Time: 120 ms, Memory: 16.3 MB - LeetHub

--- valid-sudoku/valid-sudoku.py
+++ valid-sudoku/valid-sudoku.py
@@ -1,37 +1,39 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        # row
-        for row in board:
+        
+        def valid(row):
             for key, val in Counter(row).items():
                 if key != '.' and val > 1:
                     return False
+            return True
+        
+        # row
+        for row in board:
+            if not valid(row):
+                return False
     
         # col
         for j in range(9):
-            c = Counter()
+            ls = []
             for i in range(9):
                 num = board[i][j]
-                if num == '.':
-                    continue
-                if c[num]:
-                    return False
-                c[num] += 1
+                ls.append(num)
+            if not valid(ls):
+                return False
     
         # each big cell
         for I in range(3):
             for J in range(3):
-                c = Counter() # == defaultdict(int)
+                ls = []
                 # each small cell
                 for i in range(3):
                     i += 3 * I
                     for j in range(3):
                         j += 3 * J
                         num = board[i][j]
-                        if num == '.':
-                            continue
-                        if c[num]:
-                            return False
-                        c[num] += 1
+                        ls.append(num)
+                if not valid(ls):
+                    return False
         
         return True
         
