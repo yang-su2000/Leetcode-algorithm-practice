570e8cea4418d8637b70eb06de4dece2586796da
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-09-16 22:06:48 -0400

    Time: 666 ms (17.23%), Space: 49.1 MB (7.69%) - LeetHub

--- 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.py
+++ 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.py
@@ -1,21 +1,23 @@
 class Solution:
     def shortestPathLength(self, graph: List[List[int]]) -> int:
         n = len(graph)
-        t = (1 << n) - 1
-        cache = {}
+        d = [[inf] * n for _ in range(n)]
+        for node, childs in enumerate(graph):
+            for child in childs:
+                d[node][child] = 1
+        for i in range(n):
+            d[i][i] = 0
+        for k in range(n):
+            for i in range(n):
+                for j in range(n):
+                    d[i][j] = min(d[i][j], d[i][k] + d[k][j])
+        @cache
         def dp(node, bit):
-            nonlocal n, t, cache
-            state = (node, bit)
-            if state in cache:
-                return cache[state]
-            if bit == (1 << node):
+            if bit == (1 << n) - 1:
                 return 0
-            cache[state] = inf
             ret = inf
-            for child in graph[node]:
-                if bit & (1 << child):
-                    ret = min(ret, 1 + dp(child, bit), 1 + dp(child, bit ^ (1 << node)))
-            cache[state] = ret
+            for child in range(n):
+                if bit & (1 << child) == 0:
+                    ret = min(ret, d[node][child] + dp(child, bit | (1 << child)))
             return ret
-        
-        return min(dp(node, (1 << n) - 1) for node in range(n))
+        return min(dp(node, (1 << node)) for node in range(n))
