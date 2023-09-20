43cb522876c5d4f292285760b027b6fee77a53ee
Yang Su <67517189+yang-su2000@users.noreply.github.com>
2023-09-19 21:33:44 -0400

    Time: 979 ms (59.95%), Space: 30.1 MB (98.66%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
@@ -1,42 +1,14 @@
 class Solution:
     def minOperations(self, nums: List[int], x: int) -> int:
         n = len(nums)
-        psum = [0] * n
-        psum[0] = nums[0]
-        # edge case - only first
-        if psum[0] == x:
-            return 1
         ans = n + 1
-        for i in range(1, n):
-            psum[i] = psum[i-1] + nums[i]
-            # edge case - only left
-            if psum[i] == x:
-                ans = min(ans, i + 1)
-        ssum = [0] * n
-        ssum[-1] = nums[-1]
-        for i in range(n-2, -1, -1):
-            ssum[i] = ssum[i+1] + nums[i]
-        for i in range(n-1, -1, -1):
-            remain = x - ssum[i]
-            op = n - i
-            if remain < 0:
-                continue
-            elif remain == 0:
-                ans = min(ans, op)
-                continue
-            else:
-                l = 0
-                r = i-1
-                while l < r:
-                    mid = (l + r) // 2
-                    if psum[mid] < remain:
-                        l = mid + 1
-                    elif psum[mid] == remain:
-                        l = mid
-                        break
-                    else:
-                        r = mid - 1
-                if psum[l] == remain:
-                    op += l + 1
-                    ans = min(ans, op)
+        l = 0
+        csum = sum(nums)
+        for r in range(n):
+            csum -= nums[r]
+            while csum < x and l <= r:
+                csum += nums[l]
+                l += 1
+            if csum == x:
+                ans = min(ans, l + (n - r - 1))
         return ans if ans < n + 1 else -1
