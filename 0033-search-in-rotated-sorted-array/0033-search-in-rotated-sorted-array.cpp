f5b10520845db8fc4ebc96a4c4dd8bc4752534bc
Yang Su <ys724@cornell.edu>
2023-09-26 00:11:35 -0400

    Time: 8 ms (19.15%), Space: 11.3 MB (7.04%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int l=0, r=nums.size()-1;
-        function<bool(int, int)> same_side = [&](int mid, int target) {
-            if (nums[0] <= mid && nums[0] <= target) return true;
-            if (nums[0] > mid && nums[0] > target) return true;
-            return false;
-        };
+        int n = nums.size();
+        int l = 0, r = n - 1;
         while (l < r) {
-            int mid = (l + r) / 2;
-            int val;
-            if (same_side(nums[mid], target)) val = nums[mid];
-            else if (nums[0] <= target) val = INT_MAX;
-            else val = INT_MIN;
-            if (target == val) return mid;
-            else if (target < val) r = mid-1;
-            else l = mid+1;
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[0] <= target) {
+                if (nums[0] <= nums[mid] && nums[mid] < target) l = mid + 1;
+                else if (target <= nums[mid]) r = mid;
+                else r = mid - 1;
+            } else if (target <= nums[n-1]) {
+                if (nums[mid] > nums[n-1]) l = mid + 1;
+                else if (target <= nums[mid]) r = mid;
+                else l = mid + 1;
+            } else {
+                return -1;
+            }
+            // cout << l << ", " << mid << ", " << r << endl;
+            // break;
         }
-        if (nums[l] == target) return l;
-        return -1;
+        if (nums[l] != target) return -1;
+        return l;
     }
 };
