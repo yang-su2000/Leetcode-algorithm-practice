8b6c77a159d53c2c22031d3ef80e507a9bfb7814
Yang Su <ys724@cornell.edu>
2023-10-13 20:58:12 -0400

    Time: 4918 ms (5.55%), Space: 736.5 MB (5.18%) - LeetHub

--- 2742-painting-the-walls/2742-painting-the-walls.py
+++ 2742-painting-the-walls/2742-painting-the-walls.py
@@ -1,21 +1,14 @@
 class Solution:
     def paintWalls(self, cost: List[int], time: List[int]) -> int:
-        d = defaultdict(lambda: inf)
-        d[-1] = 0
-        d[time[0]] = cost[0]
-        n = len(cost)
-        for i in range(1, n):
-            t, c = time[i], cost[i]
-            d2 = defaultdict(lambda: inf)
-            for t2, c2 in d.items():
-                d2[t2-1] = min(d2[t2-1], c2)
-                d2[t2+t] = min(d2[t2+t], c + c2)
-            # d2[-1] = min(d2[-1], 0)
-            # d2[t] = min(d2[t], c)
-            d = d2
-            # print(d)
-        ans = inf
-        for t, c in d.items():
-            if t >= 0:
-                ans = min(ans, c)
-        return ans
+        @cache
+        def dp(i, remain):
+            # print(i, remain)
+            if i == len(cost):
+                return 0 if remain >= 0 else inf
+            ret = inf
+            if remain >= -len(cost):
+                ret = min(ret, dp(i + 1, remain - 1))
+            if remain <= len(cost):
+                ret = min(ret, cost[i] + dp(i + 1, remain + time[i]))
+            return ret
+        return dp(0, 0)
