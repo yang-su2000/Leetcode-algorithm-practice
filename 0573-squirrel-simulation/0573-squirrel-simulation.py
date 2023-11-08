ffceb9f06efce05e40074fc6265aceaf9390483a
Yang Su <ys724@cornell.edu>
2023-11-08 00:29:59 -0500

    Time: 123 ms (97.56%), Space: 17.4 MB (36.59%) - LeetHub

--- 0573-squirrel-simulation/0573-squirrel-simulation.py
+++ 0573-squirrel-simulation/0573-squirrel-simulation.py
@@ -1,9 +1,6 @@
 class Solution:
     def minDistance(self, height: int, width: int, tree: List[int], s: List[int], nuts: List[List[int]]) -> int:
-        ans = 0
-        min_dist = inf
         def d(a, b): return abs(a[0] - b[0]) + abs(a[1] - b[1])
-        for nut in nuts:
-            min_dist = min(min_dist, d(s, nut) - d(tree, nut))
-            ans += 2 * d(tree, nut)
-        return ans + min_dist
+        ans = sum(d(tree, nut) for nut in nuts)
+        min_dist = min(d(s, nut) - d(tree, nut) for nut in nuts)
+        return 2 * ans + min_dist
