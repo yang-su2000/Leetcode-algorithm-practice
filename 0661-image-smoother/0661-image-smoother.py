693674ff221eb00091192958c0e187eb81ee8735
Yang Su <ys724@cornell.edu>
2023-12-19 00:57:12 -0500

    Time: 587 ms (13.49%), Space: 17 MB (59.70%) - LeetHub

--- 0661-image-smoother/0661-image-smoother.py
+++ 0661-image-smoother/0661-image-smoother.py
@@ -1,13 +1,3 @@
 class Solution:
     def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
-        n, m = len(img), len(img[0])
-        ans = [[0] * m for _ in range(n)]
-        for i in range(n):
-            for j in range(m):
-                i1, i2 = max(0, i - 1), min(n - 1, i + 1)
-                j1, j2 = max(0, j - 1), min(m - 1, j + 1)
-                for i_ in range(i1, i2 + 1):
-                    for j_ in range(j1, j2 + 1):
-                        ans[i][j] += img[i_][j_]
-                ans[i][j] //= ((i2 - i1 + 1) * (j2 - j1 + 1))
-        return ans
+        return [[sum(sum(row[max(0, j - 1): min(len(img[0]) - 1, j + 1) + 1]) for row in img[max(0, i - 1): min(len(img) - 1, i + 1) + 1]) // ((min(len(img) - 1, i + 1) - max(0, i - 1) + 1) * (min(len(img[0]) - 1, j + 1) - max(0, j - 1) + 1)) for j in range(len(img[0]))] for i in range(len(img))]
