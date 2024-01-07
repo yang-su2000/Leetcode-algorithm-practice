c0862c3b97e6de08bfabe50507e0fbd09f8b7065
Yang Su <ys724@cornell.edu>
2024-01-07 15:06:29 -0500

    Time: 66 ms (89.49%), Space: 18.4 MB (42.15%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.py
+++ 0174-dungeon-game/0174-dungeon-game.py
@@ -1,26 +1,13 @@
 class Solution:
     def calculateMinimumHP(self, d: List[List[int]]) -> int:
-        n, m = len(d), len(d[0])
-        
-        def solve(val):
-            dp = [[-inf] * m for _ in range(n)]
-            dp[0][0] = val + d[0][0]
-            for i in range(n):
-                for j in range(m):
-                    if i > 0 and dp[i-1][j] > 0:
-                        dp[i][j] = max(dp[i][j], dp[i-1][j] + d[i][j])
-                    if j > 0 and dp[i][j-1] > 0:
-                        dp[i][j] = max(dp[i][j], dp[i][j-1] + d[i][j])
-            # print(dp)
-            return dp[-1][-1] > 0
-        
-        l = 1
-        r = 399 * 1000 + 1
-        while l < r:
-            mid = (l + r) // 2
-            if solve(mid):
-                r = mid
-            else:
-                l = mid + 1
-            # print(mid)
-        return l
+        n, m = len(d), len(d[0])  
+        dp = [[inf] * m for _ in range(n)]
+        for i in range(n - 1, -1, -1):
+            for j in range(m - 1, -1, -1):
+                nex = min(dp[i+1][j] if i < n-1 else inf, dp[i][j+1] if j < m-1 else inf)
+                if nex == inf:
+                    dp[i][j] = 1 - min(0, d[i][j])
+                else:
+                    dp[i][j] = max(1, nex - d[i][j])
+        # print(dp)
+        return dp[0][0]
