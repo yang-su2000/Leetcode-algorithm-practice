735a63fef3b1cb8308d094b8e0d9aa15832786ac
Yang Su <ys724@cornell.edu>
2024-01-07 20:37:10 -0500

    Time: 66 ms (78.67%), Space: 21.6 MB (5.11%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.py
+++ 0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -1,12 +1,34 @@
 class Solution:
     def shortestPalindrome(self, s: str) -> str:
+        # return self.shortestPalindrome_rolling_hash(s)
+        return self.shortestPalindrome_KMP(s)
+    
+    def shortestPalindrome_KMP(self, s: str) -> str:
+        
+        def KMP(s):
+            n = len(s)
+            p = [0] * n
+            for i in range(1, n):
+                j = p[i-1]
+                while j and s[i] != s[j]:
+                    j = p[j-1]
+                if s[i] == s[j]:
+                    j += 1
+                p[i] = j
+            return p
+        
+        s2 = s[::-1]
+        s_total = s + "#" + s2
+        prefix_func = KMP(s_total)
+        l = prefix_func[-1]
+        return s2[:len(s)-l] + s
+    
+    def shortestPalindrome_rolling_hash(self, s: str) -> str:
         s2 = s[::-1]
         if s == s2:
             return s
-        n = len(s)
-        m = int(1e9+9)
         
-        def rolling_pw(m, p=31):
+        def rolling_pw(n, m, p=31):
             pw = [0] * n
             pw[0] = p
             for i in range(1, n):
@@ -14,7 +36,7 @@ class Solution:
             return pw
         
         def rolling_hash(s, m, pw):
-            h = [0] * n
+            h = [0] * len(s)
             for i, c in enumerate(s):
                 c = ord(c) - ord('a') + 1
                 h[i] = c * pw[i]
@@ -23,7 +45,9 @@ class Solution:
                 h[i] %= m
             return h
         
-        pw = rolling_pw(m)
+        n = len(s)
+        m = int(1e9+9)
+        pw = rolling_pw(n, m)
         prefix_h = rolling_hash(s, m, pw)
         suffix_h = rolling_hash(s2, m, pw)
         
