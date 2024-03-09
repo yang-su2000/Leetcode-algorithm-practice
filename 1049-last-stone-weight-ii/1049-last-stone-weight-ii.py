1ea415ebcb627b028cf7e8dbbb8d0950f005e493
Yang Su <ys724@cornell.edu>
2024-03-08 22:19:55 -0500

    Time: 56 ms (59.13%), Space: 16.9 MB (64.42%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.py
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.py
@@ -1,19 +1,14 @@
 class Solution:
     def lastStoneWeightII(self, stones: List[int]) -> int:
-        
-        def solve(ans):
-            dp = defaultdict(int)
-            dp[stones[0]] = 1
-            dp[-stones[0]] = 1
-            for i in stones[1:]:
-                dp2 = defaultdict(int)
-                for val, count in dp.items():
-                    dp2[val + i] += count
-                    dp2[val - i] += count
-                dp = dp2
-            return dp[ans] > 0
-        
+        dp = defaultdict(int)
+        dp[stones[0]] = 1
+        dp[-stones[0]] = 1
+        for i in stones[1:]:
+            dp2 = defaultdict(int)
+            for val, count in dp.items():
+                dp2[val + i] += count
+                dp2[val - i] += count
+            dp = dp2
         for ans in range(101):
-            if solve(ans):
+            if dp[ans]:
                 return ans
-        
