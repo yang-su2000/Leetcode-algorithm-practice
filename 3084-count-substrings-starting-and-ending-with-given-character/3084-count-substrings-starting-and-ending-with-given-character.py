bd2af7fbaead7b67d97e00db1e532927b94f80a0
Yang Su <ys724@cornell.edu>
2024-03-17 00:53:44 -0400

    Time: 42 ms (100.00%), Space: 17.5 MB (100.00%) - LeetHub

--- 3084-count-substrings-starting-and-ending-with-given-character/3084-count-substrings-starting-and-ending-with-given-character.py
+++ 3084-count-substrings-starting-and-ending-with-given-character/3084-count-substrings-starting-and-ending-with-given-character.py
@@ -1,27 +1,2 @@
 class Solution:
-    def countSubstrings(self, s: str, c: str) -> int:
-        # optimized
-        start_idxs = [] # current start indexes
-        ans = 0
-        for end_idx in range(len(s)):
-            if s[end_idx] == c:
-                start_idxs.append(end_idx)
-                ans += len(start_idxs)
-                # print(end_idx, start_idxs, ans)
-        # print()
-        # s = "abada" c = "a"
-        # end_idx = 0, start_idxs = [0], ans += 1
-        # "[a]bada"
-        # end_idx = 2, start_idxs = [0, 2], ans += 2
-        # "[aba]da", "ab[a]da"
-        # end_idx = 4, start_idxs = [0, 2, 4], ans += 3
-        # "[abada]", "ab[ada]", "abad[a]"
-        
-        # final math
-        start_idxs_count = 0 # number of current start indexes
-        ans = 0
-        for end_idx in range(len(s)):
-            if s[end_idx] == c:
-                start_idxs_count += 1
-                ans += start_idxs_count
-        return ans
+    def countSubstrings(self, s: str, c: str) -> int: return s.count(c) * (s.count(c) + 1) // 2
