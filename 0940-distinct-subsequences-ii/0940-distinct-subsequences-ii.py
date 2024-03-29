b21f89fb6b61c836a65698156cd959fe97e22d3c
Yang Su <ys724@cornell.edu>
2024-03-28 21:31:50 -0400

    Time: 47 ms (77.65%), Space: 17.4 MB (17.06%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.py
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.py
@@ -2,17 +2,18 @@ class Solution:
     def distinctSubseqII(self, s: str) -> int:
         n = len(s)
         dp = [0] * n
-        dp[0] = 1
-        c = set([s[0]])
+        p = [0] * n
+        p[0] = dp[0] = 1
+        d = {s[0]: 0}
         mod = int(1e9+7)
         for i in range(1, n):
-            for j in range(i-1, -1, -1):
-                dp[i] += dp[j]
-                if s[j] == s[i]:
-                    break
-            if s[i] not in c:
-                c.add(s[i])
+            c = s[i]
+            dp[i] = p[i-1]
+            if c in d and d[c]:
+                dp[i] -= p[d[c] - 1]
+            elif c not in d:
                 dp[i] += 1
-            dp[i] %= mod
-        # print(dp)
+            d[c] = i
+            p[i] = p[i-1] + dp[i]
+        # print(dp, p)
         return sum(dp) % mod
