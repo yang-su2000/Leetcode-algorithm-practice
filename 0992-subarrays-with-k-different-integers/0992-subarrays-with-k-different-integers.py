d533e307c7e02d9fa97d4df12e279c3a6e498576
Yang Su <ys724@cornell.edu>
2024-03-30 00:36:25 -0400

    Time: 282 ms (93.49%), Space: 20.7 MB (5.33%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.py
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.py
@@ -1,22 +1,24 @@
-from sortedcontainers import SortedSet
-
 class Solution:
     def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
         ans = 0
         n = len(nums)
-        s = SortedSet()
-        d = {}
-        j = -1
-        for i in range(n):
-            val = nums[i]
-            if val in d:
-                s.remove(d[val])
-            d[val] = i
-            s.add(d[val])
-            if len(s) > k:
-                j = s.pop(0)
-                del d[nums[j]]
-            if len(s) == k:
-                ans += s[0] - j
-            # print(i, s, ans)
+        d = defaultdict(int)
+        l = 0
+        count = 0
+        for r in range(n):
+            d[nums[r]] += 1
+            if d[nums[r]] == 1:
+                k -= 1
+            if k < 0:
+                d[nums[l]] -= 1
+                if d[nums[l]] == 0:
+                    k += 1
+                l += 1
+                count = 0
+            if k == 0:
+                while d[nums[l]] > 1:
+                    d[nums[l]] -= 1
+                    l += 1
+                    count += 1
+                ans += count + 1
         return ans
