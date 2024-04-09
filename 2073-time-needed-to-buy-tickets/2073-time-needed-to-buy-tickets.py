6a029ab380fa6fe5db50aa03d59e3ae1c38fda8c
Yang Su <ys724@cornell.edu>
2024-04-08 20:34:31 -0400

    Time: 43 ms (69.89%), Space: 16.5 MB (51.83%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
@@ -1,10 +1,3 @@
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
-        val = tickets[k]
-        ans = 0
-        for i in range(len(tickets)):
-            if i <= k:
-                ans += min(val, tickets[i])
-            else:
-                ans += max(0, min(val - 1, tickets[i]))
-        return ans
+        return sum(min(tickets[i], tickets[k] - (i > k)) for i in range(len(tickets)))
