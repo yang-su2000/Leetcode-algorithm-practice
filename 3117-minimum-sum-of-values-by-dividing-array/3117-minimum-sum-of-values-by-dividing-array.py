28fbdcee60fc13015acc0fc0a67205c314942ce1
Yang Su <ys724@cornell.edu>
2024-04-14 13:11:55 -0400

    Time: 1208 ms (50.00%), Space: 438.2 MB (50.00%) - LeetHub

--- 3117-minimum-sum-of-values-by-dividing-array/3117-minimum-sum-of-values-by-dividing-array.py
+++ 3117-minimum-sum-of-values-by-dividing-array/3117-minimum-sum-of-values-by-dividing-array.py
@@ -3,33 +3,21 @@ class Solution:
         n = len(nums)
         m = len(ands)
         limit = -1
-        dp = defaultdict(lambda: defaultdict(lambda: inf)) # m -> cval -> sval
-        dp[0][limit] = 0
-        for val in nums:
-            dp2 = defaultdict(lambda: defaultdict(lambda: inf))
-            for m_, d in dp.items():
-                if m_ >= m:
-                    continue
-                for cval, mval in d.items():
-                    cval2 = cval & val
-                    if cval2 == ands[m_]:
-                        dp2[m_ + 1][limit] = min(dp2[m_ + 1][limit], mval + val)
-                    valid = True
-                    for b in range(20):
-                        if ((cval2 >> b) & 1) or (not ((ands[m_] >> b) & 1)):
-                            pass
-                        else:
-                            valid = False
-                            break
-                    if valid:
-                        dp2[m_][cval2] = min(dp2[m_][cval2], mval)
-            dp = dp2
-            # print(val)
-            # for m_, d in dp.items():
-            #     print(m_, "->", end=" ")
-            #     for cval, mval in d.items():
-            #         print(f"[{bin(cval)}, {mval}]", end=" ")
-            #     print()
-        return -1 if dp[m][limit] == inf else dp[m][limit]
+        
+        @cache
+        def dp(idx, m_, val):
+            if idx == n:
+                return 0 if m_ == m else inf
+            if m_ == m:
+                return inf
+            val2 = val & nums[idx]
+            ret = dp(idx + 1, m_, val2)
+            if val2 == ands[m_]:
+                ret = min(ret, dp(idx + 1, m_ + 1, limit) + nums[idx])
+            # print(idx, m_, val2, ":", ret)
+            return ret
+        
+        ans = dp(0, 0, limit)
+        return ans if ans != inf else -1
         
         
