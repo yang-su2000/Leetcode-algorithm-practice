b3f638b608bd415b0580045df6844487a101e539
Yang Su <ys724@cornell.edu>
2024-04-28 22:29:04 -0400

    Time: 567 ms (92.04%), Space: 30.9 MB (94.90%) - LeetHub

--- 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.py
+++ 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.py
@@ -1,6 +1,3 @@
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
-        val = k
-        for i in nums:
-            val ^= i
-        return sum(1 for c in str(bin(val))[2:] if c == '1')
+        return reduce(xor, nums, k).bit_count()
