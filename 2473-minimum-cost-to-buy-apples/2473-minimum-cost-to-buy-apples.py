16782fea1792246e1864a3204b55ca8ca03dbd65
Yang Su <ys724@cornell.edu>
2024-05-08 02:15:48 -0400

    Time: 84 ms (77.12%), Space: 17.3 MB (70.34%) - LeetHub

--- 2473-minimum-cost-to-buy-apples/2473-minimum-cost-to-buy-apples.py
+++ 2473-minimum-cost-to-buy-apples/2473-minimum-cost-to-buy-apples.py
@@ -6,23 +6,17 @@ class Solution:
             y -= 1
             A[x].append((y, w))
             A[y].append((x, w))
-        
-        def dij(start):
-            ans = [inf] * n
-            ans[start] = appleCost[start]
-            cur = [(appleCost[start], start)]
-            while cur:
-                cost, node = heappop(cur)
-                if cost > ans[node]:
-                    continue
-                for child, w in A[node]:
-                    cost2 = cost - appleCost[node] + appleCost[child] + w * (k + 1)
-                    if cost2 < ans[child]:
-                        ans[child] = cost2
-                        heappush(cur, (cost2, child))
-            return min(ans)
-        
-        ret = [0] * n
-        for i in range(n):
-            ret[i] = dij(i)
-        return ret
+
+        ans = appleCost.copy()
+        cur = [(appleCost[i], i) for i in range(n)]
+        heapify(cur)
+        while cur:
+            cost, node = heappop(cur)
+            if cost > ans[node]:
+                continue
+            for child, w in A[node]:
+                cost2 = cost + w * (k + 1)
+                if cost2 < ans[child]:
+                    ans[child] = cost2
+                    heappush(cur, (cost2, child))
+        return ans
