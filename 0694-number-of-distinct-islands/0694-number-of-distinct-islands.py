c1526087156ce15a59db6aec66a923c6a29129a0
Yang Su <ys724@cornell.edu>
2024-06-08 23:51:18 -0400

    Time: 150 ms (73.07%), Space: 17 MB (85.62%) - LeetHub

--- 0694-number-of-distinct-islands/0694-number-of-distinct-islands.py
+++ 0694-number-of-distinct-islands/0694-number-of-distinct-islands.py
@@ -1,50 +1,23 @@
 class Solution:
     def numDistinctIslands(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        islands = []
-        
-        def dfs(x, y):
-            island = []
-            d = [(0, -1), (0, 1), (-1, 0), (1, 0)]
-            cur = [(x, y)]
-            while cur:
-                x, y = cur.pop()
-                island.append((x, y))
-                for dx, dy in d:
-                    x2, y2 = x + dx, y + dy
-                    if 0 <= x2 < m and 0 <= y2 < n and grid[x2][y2] == 1:
-                        cur.append((x2, y2))
-                        grid[x2][y2] = 0
-            return island
-        
-        for x in range(m):
-            for y in range(n):
-                if grid[x][y] == 0:
-                    continue
-                grid[x][y] = 0
-                island = dfs(x, y)
-                islands.append(island)
-        
-        # print(islands)
-        k = len(islands)
-        ans = 0
-        for i in range(k):
-            distinct = True
-            for j in range(i + 1, k):
-                island = islands[i]
-                target = islands[j]
-                if len(island) != len(target):
-                    continue
-                diff = (target[0][0] - island[0][0], target[0][1] - island[0][1])
-                same = True
-                # print(i, j, island, target)
-                for (x, y), (x2, y2) in zip(island, target):
-                    if (x2 - x) != diff[0] or (y2 - y) != diff[1]:
-                        same = False
-                        break
-                if same:
-                    distinct = False
-                    break
-            if distinct:
-                ans += 1
-        return ans
+
+        def dfs(x,y,direction):
+            if x < 0 or x>= m or y < 0 or y >=len(grid[0]) or grid[x][y] !=1:
+                return
+            grid[x][y]=0
+            path.append(direction)
+            dfs(x+1, y, 'D')
+            dfs(x-1, y, 'U')
+            dfs(x, y+1, 'R')
+            dfs(x, y-1, 'L')
+            path.append('B')
+
+        distinct_islands = set()
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    path = []
+                    dfs(i,j,'S')
+                    distinct_islands.add(tuple(path))
+        return len(distinct_islands)
